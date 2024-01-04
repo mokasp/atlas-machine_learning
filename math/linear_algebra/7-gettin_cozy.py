93f12214d5973373dbb2eda88a487ab2e465d741
mokasp <kasperbouldin@gmail.com>
2024-01-04 15:13:17 -0600

    01-04-24 | 3:13pm | remove checking for empty matrix

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,8 +6,6 @@ def cat_matrices2D(mat1, mat2, axis=0):
     mat1_y = len(mat01[0])
     mat2_x = len(mat02)
     mat2_y = len(mat02[0])
-    if mat1_x == 0 and mat2_x == 0:
-        return None
     if axis == 0 and mat1_y != mat2_y:
         return None
     if axis == 1 and mat1_x != mat2_x:
@@ -19,14 +17,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
             new_mat1 = []
             for y in range(mat1_y):
                 new_mat1.append(mat01[x][y])
-            if len(new_mat1) != 0:
-                new_mat.append(new_mat1)
+            new_mat.append(new_mat1)
         for x in range(mat2_x):
             new_mat4 = []
             for y in range(mat2_y):
                 new_mat4.append(mat02[x][y])
-            if len(new_mat4) != 0:
-                new_mat.append(new_mat4)
+            new_mat.append(new_mat4)
         return new_mat
     else:
         new_mat += mat01
