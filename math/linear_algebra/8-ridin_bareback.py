1a8120184828f487cc77e8030f475be40382acc3
mokasp <kasperbouldin@gmail.com>
2024-01-04 17:42:56 -0600

    01-04-24 | 5:43pm | return none if dimensions are not matching

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,9 +4,9 @@ def mat_mul(mat1, mat2):
     mat1_columns = len(mat1[0])
     mat2_rows = len(mat2)
     mat2_columns = len(mat2[0])
-
     mat3 = []
-
+    if mat1_columns != mat2_rows:
+        return None
     for x in range(mat1_rows):
         mat3_temp = []
         for y in range(mat2_columns):
