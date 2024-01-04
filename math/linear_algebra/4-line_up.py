81838fae0b109bdb3a0f8225dcd4774070b09705
mokasp <kasperbouldin@gmail.com>
2024-01-03 22:37:02 -0600

    01-03-24 | 10:37pm | adding elements of arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def add_arrays(arr1, arr2):
+    new_arr = []
+    if len(arr1) ==  len(arr2):
+        for i in range(len(arr1)):
+            new_arr.append(arr1[i] + arr2[i])
+        return new_arr
