33e27cd5d88d1c9e8956aaed4bf85bfe8d1134df
mokasp <kasperbouldin@gmail.com>
2024-01-03 20:27:35 -0600

    01-03-24 | 8:27pm | using recursion to find shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
 def matrix_shape(matrix):
-    mat = np.array(matrix)
-    return list(mat.shape)
+    if isinstance(matrix, list):
+        shape_list = [len(matrix)]
+        if isinstance(matrix[0], list):
+            shape_list += matrix_shape(matrix[0])
+        else:
+            shape_list += []
+    return shape_list
