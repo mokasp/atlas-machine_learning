b0a9ec1b32cc989919862ea724f352f6a8fab9d1
mokasp <kasperbouldin@gmail.com>
2024-01-04 00:53:06 -0600

    01-04-24 | 12:53am | checking if all arrays are same length

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def add_matrices2D(mat1, mat2):
-    new_mat = [[0 for _ in range(len(mat1))] for _ in range(len(mat1[0]))]
-    if len(mat1) == len(mat2):
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        new_mat = [[0 for _ in range(len(mat1))] for _ in range(len(mat1[0]))]
         for x in range(len(mat1)):
             for y in range(len(mat1[0])):
                 new_mat[x][y] = mat1[x][y] + mat2[x][y]
-    return new_mat
+        return new_mat
