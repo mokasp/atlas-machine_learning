c9f91281f2c26ad3cf96ceeb15e611111d3a6921
mokasp <kasperbouldin@gmail.com>
2024-01-05 17:09:08 -0600

    01-04-24 | 5:09pm | mopre size checking

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,10 +4,6 @@
 
 def add_matrices2D(mat1, mat2):
     """ func that returns the sum of two 2d matrices """
-    if len(mat1) == 0 or len(mat2) == 0:
-        return [[], []]
-    elif len(mat1[0]) == 0 or len(mat2[0]) == 0:
-        return [[], []]
     if get_shape(mat1) == get_shape(mat2):
         return [[mat1[x][y] + mat2[x][y]
                  for y in range(len(mat1[0]))] for x in range(len(mat1))]
@@ -17,7 +13,7 @@ def get_shape(matrix):
     """ gets shape of a matrix """
     if isinstance(matrix, list):
         shape_list = [len(matrix)]
-        if isinstance(matrix[0], list):
+        if len(matrix) != 0 and isinstance(matrix[0], list):
             shape_list += get_shape(matrix[0])
         else:
             shape_list += []
