97ccccca545dec36bda5f03f1d3975ae3105e9e6
mokasp <kasperbouldin@gmail.com>
2024-01-05 16:56:49 -0600

    01-04-24 | 4:56pm | more efficient method of adding

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,13 +4,17 @@
 
 def add_matrices2D(mat1, mat2):
     """ func that returns the sum of two 2d matrices """
-    if len(mat1) == 0 and len(mat2) == 0:
-        return None
-    elif len(mat1[0]) == 0 and len(mat2[0]) == 0:
-        return [[], []]
-    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
-        new_mat = [[0 for _ in range(len(mat1))] for _ in range(len(mat1[0]))]
-        for x in range(len(mat1)):
-            for y in range(len(mat1[0])):
-                new_mat[x][y] = mat1[x][y] + mat2[x][y]
-        return new_mat
+    if get_shape(mat1) == get_shape(mat2):
+        return [[mat1[x][y] + mat2[x][y]
+                 for y in range(len(mat1[0]))] for x in range(len(mat1))]
+
+
+def get_shape(matrix):
+    """ gets shape of a matrix """
+    if isinstance(matrix, list):
+        shape_list = [len(matrix)]
+        if isinstance(matrix[0], list):
+            shape_list += get_shape(matrix[0])
+        else:
+            shape_list += []
+    return shape_list
