1a8f68580c32f2b8251fc45b6e2914eda7fa658d
mokasp <kasperbouldin@gmail.com>
2024-01-08 19:24:24 -0600

    checking for int

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 def summation_i_squared(n):
+    if not isinstance(n, int):
+        return None
     the_list = list(range(n + 1))
     powered = map(lambda x: x ** 2, the_list)
     return sum(powered)
