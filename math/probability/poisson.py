3323d8b610633b3f5a3e5459e1b0bcc5937ec8b0
mokasp <kasperbouldin@gmail.com>
2024-01-17 18:34:11 -0600

    1-17-24 | 6:24pm | pycodestyle and documentation

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
+""" module containing class that represents a poisson distribution"""
+
+
 class Poisson():
+    """ class that represents a poisson distribution
+
+        Parameters:
+            data (list): data used to estimate distribution
+            lambtha (int): expected number of occurences in a given time
+                            period
+
+        Methods:
+            pmf(k): calculates the probability mass function of poisson
+            cdf(k): calculates the cumulative distribution function of
+                    poisson distribution
+            """
 
     def __init__(self, data=None, lambtha=1):
+        """ intializes instance of a poisson distribution
+
+            Parameters:
+                data (list): data used to estimate distribution
+                lambtha (int): expected number of occurences in a given time
+                            period
+            """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -12,8 +34,13 @@ class Poisson():
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
-    
+
     def pmf(self, k):
+        """ calculates the probability mass function of poisson distribution
+
+            Parameters:
+                k (int): number of successes
+            """
         if k < 0:
             return 0
         e = 2.7182818285
@@ -24,8 +51,13 @@ class Poisson():
         for item in fact_list:
             denom = denom * item
         return ((e ** -(mu)) * (mu ** k)) / denom
-    
+
     def cdf(self, k):
+        """ calculates the cumulative distribution function of poisson dist
+
+            Parameters:
+                k (int): number of successes
+        """
         if k < 0:
             return 0
         e = 2.7182818285
