1f614b38d77b490bed75a69a080a33dd990689ae
mokasp <kasperbouldin@gmail.com>
2024-01-17 18:31:11 -0600

    1-17-24 | 6:31pm | pycodestyle and documentation

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
+""" module containing class that represents an exponential distribution"""
+
+
 class Exponential():
+    """ class that represents a exponential distribution
+
+        Parameters:
+            data (list): data used to estimate distribution
+            lambtha (int): expected number of occurences in a given time
+                            period
+
+        Methods:
+            pdf(x): calculates the probability density function of exponential
+            cdf(x): calculates the cumulative distribution function of
+                    exponential distribution
+            """
 
     def __init__(self, data=None, lambtha=1):
+        """ intializes instance of an exponential distribution
+
+        Parameters:
+            data (list): data used to estimate distribution
+            lambtha (int): expected number of occurences in a given time
+                        period
+        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -12,15 +34,27 @@ class Exponential():
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / (sum(data) / len(data))
-    
+
     def pdf(self, x):
+        """ calculates the probability density function of
+            exponential distribution
+
+            Parameters:
+                x (int): time period
+            """
         if x < 0:
             return 0
         e = 2.7182818285
         lam = self.lambtha
         return lam * (e ** (-(lam) * x))
-    
+
     def cdf(self, x):
+        """ calculates the cumulative distribution function of
+            exponential distribution
+
+            Parameters:
+                x (int): time period
+            """
         if x < 0:
             return 0
         e = 2.7182818285
