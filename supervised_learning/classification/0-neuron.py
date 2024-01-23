c47beb6591b4b18f3e99c5d90276701b8ff93400
mokasp <kasperbouldin@gmail.com>
2024-01-23 11:14:16 -0600

    1-23-24 | 11:14am | pycodestyle

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+""" module containing class Neuron """
 import numpy as np
 
+
 class Neuron():
-    """class neuron"""
+    """class representing a single Neuron that performs binary classification
+
+        Instance Attributes:
+            W (normal dist) - the weights vector
+            b (int) - the bias
+            A (np.array?) - activated output"""
 
     def __init__(self, nx):
-        """ initialize """
+        """ initialize Neuron
+
+            Parameters:
+                nx (int) - nmber of input features """
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
