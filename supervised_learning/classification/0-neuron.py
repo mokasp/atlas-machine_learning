70b32ba9d77dbb4223c412d44461c4ffb4c2f91b
mokasp <kasperbouldin@gmail.com>
2024-01-23 11:19:18 -0600

    1-23-24 | 11:19am | edge cases

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -17,9 +17,9 @@ class Neuron():
             Parameters:
                 nx (int) - nmber of input features """
         if type(nx) is not int:
-            raise TypeError("nx must be a integer")
-        if nx < 1:
-            raise ValueError("nx must be positive")
+            raise TypeError("nx must be an integer")
+        if nx <= 0:
+            raise ValueError("nx must be a positive integer")
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
