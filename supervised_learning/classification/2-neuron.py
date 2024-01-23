c66c059f5ea0fff206eefce60744b314931a57ac
mokasp <kasperbouldin@gmail.com>
2024-01-23 00:04:08 -0600

    1-23-24 | 12:04am | perform one forward pass

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import numpy as np
+
+class Neuron():
+    """ class neuron """
+
+    def __init__(self, nx):
+        """ initialize [] """
+        if type(nx) is not int:
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+    
+    @property
+    def W(self):
+        """ getter for [] """
+        return self.__W
+    
+    @property
+    def b(self):
+        """ getter for [] """
+        return self.__b
+    
+    @property
+    def A(self):
+        """ getter for [] """
+        return self.__A
+    
+    def sigmoid(self, z):
+        """ """
+        return 1 / (1 + np.exp(-z))
+    
+    def forward_prop(self, X):
+        """ """
+        z2 = np.dot(self.__W[0], X) + self.__b
+        self.__A = self.sigmoid(z2)
+        return self.__A
