26503d5c4e0866e075470e6ce84689491f6cf9be
mokasp <kasperbouldin@gmail.com>
2024-01-27 03:09:47 -0600

    1-27-24 | 3:09am | added forward pass class

---
+++ supervised_learning/classification/18-deep_neural_network.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" module containing class NeuralNetwork """
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """class representing a Deep Neural Network with multiple hidden layer that
+        performs binary classification
+
+        Instance Attributes:
+            L (): number of layers
+            cache (): all intermediary values
+            weights (dict): all weights and biases
+            """
+
+    def __init__(self, nx,  layers):
+        """ initialize Deep Neural Network
+
+            Parameters:
+                nx (int) - nmber of input features
+                layers (int) - number of nodes in the hidden layer """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        layers.insert(0, nx)
+        self.__cache = {}
+        self.__weights = {}
+        self.__weights = {}
+        for l in range (1, self.__L + 1):
+            if layers[l] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.__weights["W" + str(l)] = np.random.randn(layers[l], layers[l - 1]) * np.sqrt(2.0 / (layers[l- 1]))
+            self.__weights["b" + str(l)] = np.zeros((layers[l], 1))
+
+    @property
+    def L(self):
+        """ getter for [] """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ getter for [] """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ getter for [] """
+        return self.__weights
+
+    def sigmoid(self, z):
+        """ sigmoid function """
+        return 1 / (1 + np.exp(-z))
+
+    def forward_prop(self, X):
+        """ one forward pass of neuron """
+        self.__cache["A0"] = X
+        A = X
+        for l in range(1, self.__L + 1):
+            W = self.__weights["W" + str(l)]
+            b = self.__weights["b" + str(l)]
+            z = np.dot(W, A) + b
+            A = self.sigmoid(z)
+            self.__cache["A" + str(l)] = A
+        return A, self.__cache
