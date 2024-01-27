132e5f91c00189c22f633a8f8f19f55c12bbcdbd
mokasp <kasperbouldin@gmail.com>
2024-01-27 02:35:42 -0600

    1-27-24 | 2:35am || add condition

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -23,7 +23,7 @@ class DeepNeuralNetwork():
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         layers.insert(0, nx)
