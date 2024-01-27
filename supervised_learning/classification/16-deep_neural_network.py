a6e409ef05dad3cdd8bc3d86df16a259fb53b512
mokasp <kasperbouldin@gmail.com>
2024-01-27 01:50:18 -0600

    1-27-24 | 1:50 am | deep NN w dictionary of weights and biases

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" module containing class NeuralNetwork """
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """class representing a Deep Neural Network with multiple hidden layer that
+        performs binary classification
+
+        Instance Attributes:
+            L (): number of layers
+            cache (): all intermediary values
+            weights (dict): all weights and biases
+            """
+
+    def __init__(self, nx,  layers):
+        """ initialize Deep Neural Network
+
+            Parameters:
+                nx (int) - nmber of input features
+                layers (int) - number of nodes in the hidden layer """
+        self.L = len(layers)
+        layers.insert(0, nx)
+        print(layers)
+        self.cache = {}
+        self.weights = {}
+        self.weights = {}
+        for l in range (1, self.L + 1):
+            print((layers[l], layers[l-1]))
+            self.weights["W" + str(l)] = np.random.randn(layers[l], layers[l - 1]) * np.sqrt(2.0 / (layers[l- 1]))
+            self.weights["b" + str(l)] = np.zeros((layers[l], 1))
