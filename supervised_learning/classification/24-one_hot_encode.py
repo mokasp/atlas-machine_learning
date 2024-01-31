a5a64d0b2c3dbf98f210c34330be69a9c742a3f7
mokasp <kasperbouldin@gmail.com>
2024-01-31 05:12:56 -0600

    1-31-24 | 5:13 am | edge cases

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ function that decodes a one hot encoded vector """
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+        return None
+    if classes < 2 or classes < Y[max(Y - 1)]:
+        return None
     encoded = np.zeros((classes, len(Y)))
     for x, y in enumerate(Y):
         encoded[y][x] = 1
