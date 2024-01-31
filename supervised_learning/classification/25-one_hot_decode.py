82cc96e830f7764176afabae79b658ac9e59369e
mokasp <kasperbouldin@gmail.com>
2024-01-31 00:53:29 -0600

    1-31 |12:53pm | none on error

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -3,8 +3,11 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     decoded = []
-    for x in range(one_hot.shape[1]):
-        for y in range(len(one_hot.shape[0])):
-            if one_hot[y][x] == 1:
-                decoded.append(y)
-    return decoded
+    try:
+        for x in range(one_hot.shape[1]):
+            for y in range(len(one_hot.shape[0])):
+                if one_hot[y][x] == 1:
+                    decoded.append(y)
+        return decoded
+    except:
+        return None
