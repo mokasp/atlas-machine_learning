196c16b2257d0bb6ceeca03f9b0ae7aedf2b47b9
mokasp <kasperbouldin@gmail.com>
2024-01-30 23:54:22 -0600

    1-30-24 | 11:54pm | fixing rows and columns

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 def one_hot_encode(Y, classes):
-    encoded = np.zeros((len(Y), classes), int)
+    encoded = np.zeros((classes, len(Y)))
     for x, y in enumerate(Y):
         encoded[y][x] = 1
     return encoded
