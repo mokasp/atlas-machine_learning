116519eea0a855c111b0bda9d799ef2b27aa110e
mokasp <kasperbouldin@gmail.com>
2024-02-02 20:51:03 -0600

    2-2-24 8:51pm | using metrics

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
 
-
 def calculate_accuracy(y, y_pred):
-    Acc = y - y_pred
-    return tf.reduce_mean(Acc)
+    m = tf.keras.metrics.Accuracy()
+    _ = m.update_state(y, y_pred)
+    return tf.reduce_mean(m.result())
