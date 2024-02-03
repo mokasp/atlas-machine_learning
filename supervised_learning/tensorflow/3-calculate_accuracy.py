0a712186cc85924964736ef46427fff088f82b9b
mokasp <kasperbouldin@gmail.com>
2024-02-02 21:16:11 -0600

    2-2-24 9:16pm | using math module

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -2,4 +2,7 @@
 import tensorflow.compat.v1 as tf
 
 def calculate_accuracy(y, y_pred):
-    return tf.reduce_mean(tf.argmax(y, axis=1) - tf.argmax(y_pred, axis=1))
+    pred = tf.math.argmax(y_pred, axis=1)
+    act = tf.math.argmax(y, axis=1)
+    equality = tf.math.equal(pred, act)
+    return tf.math.reduce_mean(tf.cast(equality, tf.float32))
