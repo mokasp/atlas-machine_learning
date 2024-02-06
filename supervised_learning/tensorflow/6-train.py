32849f770016f19b8e8e1c4a8c629dfc31a94f65
mokasp <kasperbouldin@gmail.com>
2024-02-06 00:16:26 -0600

    2-6-24 12:16am | fixing save method?

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -9,25 +9,24 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     forward_prop = __import__('2-forward_prop').forward_prop
 
     
-    graph = tf.Graph()
-    with graph.as_default():
-        x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-        y_pred = forward_prop(x, layer_sizes, activations)
-        accuracy = calculate_accuracy(y, y_pred)
-        loss = calculate_loss(y, y_pred)
-        train_op = create_train_op(loss, alpha)
 
-        tf.compat.v1.add_to_collection('x', x)
-        tf.compat.v1.add_to_collection('y', y)
-        tf.compat.v1.add_to_collection('y_pred', y_pred)
-        tf.compat.v1.add_to_collection('loss', loss)
-        tf.compat.v1.add_to_collection('accuracy', accuracy)
-        tf.compat.v1.add_to_collection('train_op', train_op)
-        init = tf.global_variables_initializer()
-        saver = tf.train.Saver()
-    with tf.compat.v1.Session(graph=graph) as sess:
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+
+    tf.compat.v1.add_to_collection('x', x)
+    tf.compat.v1.add_to_collection('y', y)
+    tf.compat.v1.add_to_collection('y_pred', y_pred)
+    tf.compat.v1.add_to_collection('loss', loss)
+    tf.compat.v1.add_to_collection('accuracy', accuracy)
+    tf.compat.v1.add_to_collection('train_op', train_op)
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
         sess.run(init)
-        for i in range(0, iterations + 1):
+        for i in range(iterations + 1):
             c_tloss = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             c_tacc = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             c_vloss = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
@@ -39,8 +38,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
                 print(f'\tTraining Accuracy: {c_tacc}')
                 print(f'\tValidation Cost: {c_vloss}')
                 print(f'\tValidation Accuracy: {c_vacc}')
-            
+                
             trained = sess.run(train_op, feed_dict={x: X_train, y: Y_train})
 
-    save_p = saver.save(sess, save_path)
+        save_p = saver.save(sess, save_path)
     return save_p
