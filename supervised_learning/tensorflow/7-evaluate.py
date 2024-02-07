916b05a1ef1e42e13540daea9fd706e0cc1acfd0
mokasp <kasperbouldin@gmail.com>
2024-02-07 17:19:33 -0600

    2-7-24 5:19pm | ?? why is y_pred not showing up

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -7,12 +7,13 @@ def evaluate(X, Y, save_path):
     """ function that evaluates the output of a neural network"""
     saver = tf.train.import_meta_graph('model.ckpt.meta')
     graph = tf.get_default_graph()
-    x = tf.get_collection("x")[0]
-    y = tf.get_collection("y")[0]
+    x = graph.get_tensor_by_name("x:0")
+    y = graph.get_tensor_by_name("y:0")
     y_pred = tf.get_collection("y_pred")[0]
     accuracy = tf.get_collection("accuracy")[0]
     loss = tf.get_collection("loss")[0]
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         model = saver.restore(sess, save_path)
-        return sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        y_p, acc, los = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        return y_p, acc, los
