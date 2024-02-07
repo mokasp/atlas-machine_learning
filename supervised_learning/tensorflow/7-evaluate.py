980c129fb5bc6f86fdb1c085fa2fea14cc67ff09
mokasp <kasperbouldin@gmail.com>
2024-02-07 17:12:21 -0600

    2-7-24 5:12pm | get variables outside of session

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -5,14 +5,14 @@ import tensorflow.compat.v1 as tf
 
 def evaluate(X, Y, save_path):
     """ function that evaluates the output of a neural network"""
+    saver = tf.train.import_meta_graph('model.ckpt.meta')
+    graph = tf.get_default_graph()
+    x = tf.get_collection("x")[0]
+    y = tf.get_collection("y")[0]
+    y_pred = tf.get_collection("y_pred")[0]
+    accuracy = tf.get_collection("accuracy")[0]
+    loss = tf.get_collection("loss")[0]
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        saver = tf.train.import_meta_graph('model.ckpt.meta')
         model = saver.restore(sess, save_path)
-        graph = tf.get_default_graph()
-        x = tf.get_collection("x")[0]
-        y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        accuracy = tf.get_collection("accuracy")[0]
-        loss = tf.get_collection("loss")[0]
         return sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
