ae1da06b99992ac47d5732ba4f838cd762aa6934
mokasp <kasperbouldin@gmail.com>
2024-02-07 15:34:26 -0600

    2-7-24 3:34pm | restoring model :)

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" module cantaining function that evaluates the output of a neural network"""
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """ function that evaluates the output of a neural network"""
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.import_meta_graph('model.ckpt.meta')
+        model = saver.restore(sess, save_path)
+        graph = tf.get_default_graph()
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        return sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
