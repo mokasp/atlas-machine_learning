e80b19d460a69d9430c8a8ea5970c766beb46b8f
mokasp <kasperbouldin@gmail.com>
2024-02-07 18:04:18 -0600

    2-7-24 6:04 | all under sessipn

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -5,13 +5,13 @@ import tensorflow.compat.v1 as tf
 
 def evaluate(X, Y, save_path):
     """ function that evaluates the output of a neural network"""
-    saver = tf.train.import_meta_graph(f'{save_path}.meta')
-    x = tf.get_collection("x")[0]
-    y = tf.get_collection("y")[0]
-    y_pred = tf.get_collection("y_pred")[0]
-    accuracy = tf.get_collection("accuracy")[0]
-    loss = tf.get_collection("loss")[0]
     with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(f'{save_path}.meta')
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
         saver.restore(sess, save_path)
         y_p, acc, los = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
     return y_p, acc, los
