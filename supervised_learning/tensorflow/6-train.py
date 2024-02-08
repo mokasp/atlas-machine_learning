168763a88d2c501ec1b8e131cb54fdded0c5cefa
mokasp <kasperbouldin@gmail.com>
2024-02-07 23:30:17 -0600

    2-7-24 11:30pm | documentation and pycodestyle

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
+""" module containing function that fully trains a model"""
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
+          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """ function that fully trains a model
+
+        Parameters:
+            X_train [numpy.ndarry] - training input data
+            Y_train [numpy.ndarry] - training labels
+            X_valid [numpy.ndarry] - validation input data
+            Y_valid [numpy.ndarry] - validation labels
+            layer_sizes [list] - list containing the number of nodes in
+                                    each layer of the network
+            activations [list] - list containing the activation functions for
+                                    each layer of the network
+            loss [float] -  loss of the networks prediction
+            alpha [float] - learning rate
+            save_path [sting] - file path to save the model to
+
+        Returns:
+            save_path [string] - the path where the model was saved to
+        """
+    create_p = __import__('0-create_placeholders').create_placeholders
     calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
     calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = __import__('0-create_placeholders').create_placeholders
+    create_placeholders = create_p
     create_train_op = __import__('5-create_train_op').create_train_op
     forward_prop = __import__('2-forward_prop').forward_prop
-
-    
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
@@ -26,6 +46,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(init)
+        diction = {x: X_train, y: Y_train}
         for i in range(iterations + 1):
             c_tloss = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             c_tacc = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
@@ -39,8 +60,8 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
                 print(f'\tValidation Cost: {c_vloss}')
                 print(f'\tValidation Accuracy: {c_vacc}')
 
-            if i < iterations:    
-                trained = sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+            if i < iterations:
+                trained = sess.run(train_op, feed_dict=diction)
 
         save_p = saver.save(sess, save_path)
     return save_p
--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -4,7 +4,19 @@ import tensorflow.compat.v1 as tf
 
 
 def evaluate(X, Y, save_path):
-    """ function that evaluates the output of a neural network"""
+    """ function that evaluates the predicition of restored model
+
+        Parameters:
+            X [numpy.ndarry] -  input data to evaluate
+            Y [numpy.ndarry] - one hot encoded output labels
+            save_path [sting] - file path to load model from
+
+        Returns:
+            y_pred [numpy.ndarry] - the prediction of the network
+            accuracy [float] - tensor containing the decimal accuracy
+                                of the prediction
+            loss [float] - tensor containing the loss of the prediction
+        """
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(f'{save_path}.meta')
         x = tf.get_collection("x")[0]
@@ -13,5 +25,6 @@ def evaluate(X, Y, save_path):
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         saver.restore(sess, save_path)
-        y_p, acc, los = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        feed_dict = {x: X, y: Y}
+        y_p, acc, los = sess.run([y_pred, accuracy, loss], feed_dict=feed_dict)
     return y_p, acc, los
