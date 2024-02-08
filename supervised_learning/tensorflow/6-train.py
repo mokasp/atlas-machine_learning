a590a4659ba4b8712560f43556ce2eba3d5f9418
mokasp <kasperbouldin@gmail.com>
2024-02-07 23:46:02 -0600

    ????

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
+""" module containing function that fully trains a model and calculates the
+    loss and accuracy """
 import tensorflow.compat.v1 as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
-""" module containing function that fully trains a model and calculates the
-    loss and accuracy """
 
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
