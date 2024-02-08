b3e99a23af50e696e30e42a595f4d670688dfa7e
mokasp <kasperbouldin@gmail.com>
2024-02-07 23:44:47 -0600

    /????

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
 """ module containing function that fully trains a model and calculates the
     loss and accuracy """
 
@@ -24,12 +29,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
         Returns:
             save_path [string] - the path where the model was saved to
         """
-    create_p = '0-create_placeholders'
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = __import__(create_p).create_placeholders
-    create_train_op = __import__('5-create_train_op').create_train_op
-    forward_prop = __import__('2-forward_prop').forward_prop
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
     accuracy = calculate_accuracy(y, y_pred)
