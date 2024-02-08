2e8bcab8975701c73ed89257310e19efbdfe6abd
mokasp <kasperbouldin@gmail.com>
2024-02-07 23:33:27 -0600

    2-7-24 11:33 | pycodestyle

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -3,6 +3,13 @@ import tensorflow.compat.v1 as tf
 """ module containing function that fully trains a model"""
 
 
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """ function that fully trains a model
@@ -23,12 +30,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
         Returns:
             save_path [string] - the path where the model was saved to
         """
-    create_p = __import__('0-create_placeholders').create_placeholders
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    create_placeholders = create_p
-    create_train_op = __import__('5-create_train_op').create_train_op
-    forward_prop = __import__('2-forward_prop').forward_prop
 
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
