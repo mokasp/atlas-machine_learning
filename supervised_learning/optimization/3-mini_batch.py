0fd24eb6960b42f6d8e7e104e9ee16022590b0d2
mokasp <kasperbouldin@gmail.com>
2024-02-13 19:43:58 -0600

    2-12-24 | 7:44pm | move restore

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -36,13 +36,13 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     """
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(f'{load_path}.meta')
+        saver.restore(sess, load_path)
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
         y_pred = tf.get_collection("y_pred")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         train_op = tf.get_collection("train_op")[0]
-        saver.restore(sess, load_path)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
