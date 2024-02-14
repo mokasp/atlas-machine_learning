3894e6a6a06e7bd4b48fb58ea5f891f7a95b8f6d
mokasp <kasperbouldin@gmail.com>
2024-02-13 20:43:18 -0600

    2-13-24 | 8:43pm | remove getting y_pred from collection

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -39,7 +39,6 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         saver.restore(sess, load_path)
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         train_op = tf.get_collection("train_op")[0]
