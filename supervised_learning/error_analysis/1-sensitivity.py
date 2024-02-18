a471db9f050ddd02f6601ab7032ed4922cff18b2
mokasp <kasperbouldin@gmail.com>
2024-02-18 04:24:28 -0600

    2-18-24 | 4:24am | sensitivity

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" module containing function that calculates the sensitivity for each class
+    in a confusion matrix """
+import numpy as np
+
+
+def sensitivity(confusion):
+    """ function that calculates the sensitivity for each class in a
+        confusion matrix
+
+
+        PARAMETERS
+        ==========
+        confusion [np.ndarray]: confusion array of shape (classes, classes)
+                                with row indices representing the correct
+                                labels and column indices representing the
+                                predicted labels
+                                    > classes [int]: number of classes
+
+
+        RETURNS
+        =======
+        [numpy.ndarray]: array of shape (classes,) containing the
+                            sensitivity of each class
+    """
+    sense = []
+    for i in range(len(confusion)):
+        sense.append(confusion[i][i] / np.sum(confusion[i]))
+    return sense
