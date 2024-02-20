4330bf4610e1fc842a255ede1cbf9cab3c44dc18
mokasp <kasperbouldin@gmail.com>
2024-02-20 02:11:08 -0600

    2-20-24 | 2:11am | dropout with tensorflow

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" module containing function that creates a layer of a neural network using
+    dropout """
+import tensorflow.compat.v1 as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob):
+    """ function that creates a layer of a neural network using dropout
+
+
+        PARAMETERS
+        ==========
+        prev [tensor]: the output of the previous layer
+
+        n [int]: number of nodes the new layer should contain
+
+        activation [string]: activation function that should be used on the
+                                layer
+
+        keep_prob [?]: probability that a node will be kept
+
+
+        RETURNS
+        =======
+        [?]: output of new layer
+    """
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        name="layer",
+        kernel_initializer=init
+    )
+    tf.layers.Dropout(keep_prob)
+    return layer(prev)
