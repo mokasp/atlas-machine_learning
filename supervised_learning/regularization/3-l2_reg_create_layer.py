2d6378ac4fd712eb88626b1cc4fa4284b6005c85
mokasp <kasperbouldin@gmail.com>
2024-02-19 18:57:14 -0600

    2-19-24 | 6:57pm | making layer w l2 regularization

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" module containing function that creates a tensorflow layer that includes L2
+    regularization """
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """ function that creates a tensorflow layer that includes L2
+        regularization
+
+
+        PARAMETERS
+        ==========
+        prev [tensor]: the output of the previous layer
+
+        n [int]: number of nodes the new layer should contain
+
+        activation [string]: activation function that should be used on the
+                                layer
+
+        lambtha [?]: L2 regularization parameter
+
+
+        RETURNS
+        =======
+        [?]: output of the new layer
+    """
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        name="layer",
+        kernel_initializer=init,
+        kernel_regularizer=tf.keras.regularizers.L2(lambtha)
+    )
+    return layer(prev)
