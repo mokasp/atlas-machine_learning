adb8dc38afa359511b3f0507545bc19d04fa4219
mokasp <kasperbouldin@gmail.com>
2024-02-24 17:32:48 -0600

    2-24-24 | 5:32pm | none of this is working

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ module containing function that sets up Adam optimization for a keras
     model with categorical crossentropy loss and accuracy metrics """
+import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
@@ -18,3 +19,7 @@ def optimize_model(network, alpha, beta1, beta2):
         =======
             None
     """
+    adam = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(loss='categorical_crossentropy', optimizer=adam,
+                    metrics=['accuracy'])
+    return None
