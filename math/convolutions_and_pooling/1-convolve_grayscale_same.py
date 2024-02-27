182083613306c3fda371166a37dc44a4b8c4eac7
mokasp <kasperbouldin@gmail.com>
2024-02-27 03:30:30 -0600

    2-27-24 | 3:30am | removed print statemtns

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -17,13 +17,11 @@ def convolve_grayscale_same(images, kernel):
     """
     kern1 = kernel.shape[0]
     kern2 = kernel.shape[1]
-    print(kern1, kern2)
     m = images.shape[0]
     h = images.shape[1]
     w = images.shape[2]
     pad_tb = int(round((kern1 - 1) / 2))
     pad_lr = int(round((kern2 - 1) / 2))
-    print(pad_tb, pad_lr)
     op = np.zeros((m, h, w))
     pad = np.pad(images, ((0, 0), (pad_tb, pad_tb), (pad_lr, pad_lr)),
                  mode='constant')
