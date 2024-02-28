9488061f17dd5f2a9e02209dc61067f86946ea3a
mokasp <kasperbouldin@gmail.com>
2024-02-28 16:27:29 -0600

    2-28-24 | 4:27pm | fixed same and reg padding and pycode style

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -19,16 +19,18 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     m, h, w, c = images.shape
     sh, sw = stride
     if padding == 'same':
-        op = np.zeros((m, h, w))
+        op = np.zeros((m, h, w, kern4))
         p_h = int(((h * sh) - h + kern1 - sh) / 2) + 1
         p_w = int(((w * sw) - w + kern2 - sw) / 2) + 1
         pad = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w), (0, 0)),
                      mode='constant')
-        for row in range(h):
-            for col in range(w):
-                cur = pad[:, row*sh:row*sh+kern1, col*sw:col*sw+kern2, :]
-                op[:, row, col] = np.sum(np.multiply(cur, kernels),
-                                         axis=(1, 2, 3))
+        for filt in range(kernels.shape[3]):
+            for row in range(h):
+                for col in range(w):
+                    cur = pad[:, row*sh:row*sh+kern1, col*sw:col*sw+kern2, :]
+                    op[:, row, col] = np.sum(
+                        np.multiply(cur, kernels[:, :, :, filt]),
+                        axis=(1, 2, 3))
         return op
 
     elif padding == 'valid':
@@ -49,12 +51,14 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
         ph, pw = padding
         new_h = int(round((h - kern1 + (2 * ph) + sh) / sh)) - 1
         new_w = int(round((w - kern2 + (2 * pw) + sw) / sw)) - 1
-        op = np.zeros((m, new_h, new_w))
-        pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+        op = np.zeros((m, new_h, new_w, kern4))
+        pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                      mode='constant')
-        for row in range(new_h):
-            for col in range(new_w):
-                cur = pad[:, row*sh:row*sh+kern1, col*sw:col*sw+kern2, :]
-                op[:, row, col] = np.sum(np.multiply(cur, kernels),
-                                         axis=(1, 2, 3))
+        for filt in range(kern4):
+            for row in range(new_h):
+                for col in range(new_w):
+                    cur = pad[:, row*sh:row*sh+kern1, col*sw:col*sw+kern2, :]
+                    op[:, row, col] = np.sum(
+                        np.multiply(cur, kernels[:, :, :, filt]),
+                        axis=(1, 2, 3))
         return op
