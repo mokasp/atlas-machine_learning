f3d4d6a1e2ac268530ef543cab64a37992396917
mokasp <kasperbouldin@gmail.com>
2024-02-28 12:23:16 -0600

    2-28-24 | 12:23pm | fixed unpacking error and specify no padding for extra param

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -16,13 +16,13 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
             []:
     """
     kern1, kern2, kern3 = kernel.shape
-    m, h, w = images.shape
+    m, h, w, c = images.shape
     sh, sw = stride
     if padding == 'same':
         op = np.zeros((m, h, w))
         p_h = int(round(((h * sh) - h + kern1 - sh) / 2)) + 1
         p_w = int(round(((w * sw) - w + kern2 - sw) / 2)) + 1
-        pad = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w)),
+        pad = np.pad(images, ((0, 0), (p_h, p_h), (p_w, p_w), (0, 0)),
                      mode='constant')
         for row in range(h):
             for col in range(w):
@@ -47,7 +47,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         new_h = int(round((h - kern1 + (2 * ph) + sh) / sh)) - 1
         new_w = int(round((w - kern2 + (2 * pw) + sw) / sw)) - 1
         op = np.zeros((m, new_h, new_w))
-        pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw)),
+        pad = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                      mode='constant')
         for row in range(new_h):
             for col in range(new_w):
