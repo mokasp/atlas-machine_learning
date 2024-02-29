5a1810283053a16daf02b18764557c3613adb118
mokasp <kasperbouldin@gmail.com>
2024-02-28 19:16:54 -0600

    pooling

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" module containing function that """
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """ function that
+
+        PARAMETERS
+        ==========
+            []:
+            []:
+
+        RETURNS
+        =======
+            []:
+    """
+    k_h, k_w = kernel_shape
+    m, h, w, c = images.shape
+    sh, sw = stride
+    op_h = int(h / k_h)
+    op_w = int(w / k_w)
+    pooled = np.zeros((m, op_h, op_w, c))
+    for row in range(op_h):
+        for col in range(op_w):
+            cur = images[:, row*sh:row*sh+k_h, col*sw:col*sw+k_w]
+            if mode == 'avg':
+                pooled[:, row, col] = np.mean(cur, axis=(1, 2))
+            else:
+                pooled[:, row, col] = np.max(cur, axis=(1, 2))
+    return pooled
