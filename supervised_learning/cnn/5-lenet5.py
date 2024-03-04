127bf0880e7c77d68675aba4ce9796f08e51833a
mokasp <kasperbouldin@gmail.com>
2024-03-04 01:47:29 -0600

    lenet5 w keras first lol

---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    model = K.Sequential()
+
+    model.add(K.layers.Conv2D(kernel_initializer=K.initializers.HeNormal(), filters=6, kernel_size=(5, 5), padding='same', activation='relu', input_shape=X.shape[1:]))
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(K.layers.Conv2D(kernel_initializer=K.initializers.HeNormal(), filters=16, kernel_size=(5, 5), padding='valid', activation='relu'))
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    model.add(K.layers.Flatten())
+
+    model.add(K.layers.Dense(kernel_initializer=K.initializers.HeNormal(), units=120, activation='relu'))
+
+    model.add(K.layers.Dense(kernel_initializer=K.initializers.HeNormal(), units=84, activation='relu'))
+
+    model.add(K.layers.Dense(kernel_initializer=K.initializers.HeNormal(), units=10, activation = 'softmax'))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
