f2a9acb32bfaa1a1ec404412e728c4d5d00aa7cb
mokasp <kasperbouldin@gmail.com>
2024-03-03 20:40:39 -0600

    remove division by sample size

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -11,21 +11,19 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
         pad_h = 0
         pad_w = 0
     else:
-        pad_h = int(((h_prev * sh) - h_prev + kh - sh) / 2)
-        pad_w = int(((w_prev * sw) - w_prev + kh - sw) / 2)
+        pad_h = int(np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2))
+        pad_w = int(np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2))
         A_prev = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
-                     mode='constant')
-    dW = np.zeros(W.shape)
+                     mode='constant', constant_values=0)
+    dW = np.zeros_like(W)
     dX = np.zeros(A_prev.shape)
-    db = np.zeros((1, 1, 1, c_new))
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
     for sample in range(m):
         for row in range(h_new):
             for col in range(w_new):
                 for i in range(c_new):
-                    dX[:, row*sh:row*sh+kh, col*sw:col*sw+kw, :] += dZ[sample, row, col, i] * W[:, :, :, i]
+                    dX[sample, row*sh:row*sh+kh, col*sw:col*sw+kw, :] += dZ[sample, row, col, i] * W[:, :, :, i]
 
                     dW[:, :, :, i] += A_prev[sample, row*sh:row*sh+kh, col*sw:col*sw+kw, :] * dZ[sample, row, col, i]
-
-                    db[:, :, :, i] += dZ[sample, row, col, i]
 
     return dX, dW, db
