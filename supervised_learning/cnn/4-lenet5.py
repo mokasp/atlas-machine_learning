77f565443b4bdd860c19404cc592eeeaf0ca3203
mokasp <kasperbouldin@gmail.com>
2024-03-06 21:31:11 -0600

    pycode style

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python3
 import tensorflow.compat.v1 as tf
+""" module containing function that b uilds a modified version of the LeNet-5
+    architecture using TensorFlow. """
 
 
 def lenet5(x, y):
+    """ function that b uilds a modified version of the LeNet-5 architecture
+        using TensorFlow.
+
+    PARAMETERS
+    ==========
+        x [tf.placeholder]: Input images for the network shape (m, 28, 28, 1)
+            m - number of images
+
+        y [tf.placeholder]: One-hot labels for the network of Shape (m, 10).
+
+    RETURNS
+    =======
+        Tuple of tensors and operations:
+            - output: Tensor for the softmax activated output.
+            - train_op: Training operation that utilizes Adam optimization.
+            - loss: Tensor for the loss of the network.
+            - accuracy: Tensor for the accuracy of the network.
+    """
     init = tf.keras.initializers.VarianceScaling(scale=2.0)
     conv1 = tf.layers.Conv2D(
-        kernel_initializer=init, filters=6, kernel_size=5, padding='same', activation='relu')(x)
+        kernel_initializer=init, filters=6, kernel_size=5, padding='same',
+        activation='relu')(x)
 
     maxpool1 = tf.layers.MaxPooling2D(2, 2)(conv1)
 
     conv2 = tf.layers.Conv2D(
-        kernel_initializer=init, filters=16, kernel_size=5, padding='valid', activation='relu')(maxpool1)
+        kernel_initializer=init, filters=16, kernel_size=5, padding='valid',
+        activation='relu')(maxpool1)
 
     maxpool2 = tf.layers.MaxPooling2D(2, 2)(conv2)
 
     maxpool2_flat = tf.layers.Flatten()(maxpool2)
 
-    dense1 =tf.layers.Dense(kernel_initializer=init, units=120, activation='relu')(maxpool2_flat)
+    dense1 = tf.layers.Dense(kernel_initializer=init, units=120,
+                             activation='relu')(maxpool2_flat)
 
-    dense2 =tf.layers.Dense(kernel_initializer=init, units=84, activation='relu')(dense1)
+    dense2 = tf.layers.Dense(kernel_initializer=init, units=84,
+                             activation='relu')(dense1)
 
     logits = tf.layers.Dense(kernel_initializer=init, units=10)(dense2)
 
