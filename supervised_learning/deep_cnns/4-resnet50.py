a9245f2e9a4c005b5eaddc5eb2e363c968ef48fe
mokasp <kasperbouldin@gmail.com>
2024-03-13 23:25:55 -0500

    pycodestyle

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -44,5 +44,6 @@ def resnet50():
 
     x6 = K.layers.AveragePooling2D((7, 7), padding='same')(x5)
 
-    x7 = K.layers.Dense(1000, activation='softmax', kernel_initializer=init)(x6)
+    x7 = K.layers.Dense(1000, activation='softmax',
+                        kernel_initializer=init)(x6)
     return K.Model(inp, x7)
