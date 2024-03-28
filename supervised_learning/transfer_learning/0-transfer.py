e4a681fd575092208e321026b77e72632e4dd2cd
mokasp <kasperbouldin@gmail.com>
2024-03-27 22:03:56 -0500

    removed tensorflwo import, use keras backend for resizing

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -2,7 +2,6 @@
 """ module containing function that uses transfer learning to construct a
 model that identifies images in the CIFAR-10 dataset """
 import tensorflow.keras as K
-import tensorflow as tf
 
 
 def preprocess_data(X, Y):
@@ -34,8 +33,12 @@ def main():
                                                   weights='imagenet')
     base_model.trainable = False
     inputs = K.Input(shape=(32, 32, 3))
-    scale = K.layers.Lambda(lambda x: tf.image.resize(x, (229, 229)))(inputs)
-    x = base_model(scale, training=False)
+    sc = K.layers.Lambda(lambda x:
+                         K.backend.resize_images(
+                             x, 229 // 32, 229 // 32,
+                             data_format="channels_last",
+                             interpolation="bilinear"))(inputs)
+    x = base_model(sc, training=False)
 
     cbs = []
     save = K.callbacks.ModelCheckpoint('cifar10.h5', monitor='val_accuracy',
@@ -44,7 +47,7 @@ def main():
     e_s = K.callbacks.EarlyStopping(patience=6, restore_best_weights=True)
     cbs.append(e_s)
     lrr = K.callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.1,
-                                        patience=3, min_lr=0.00000001)
+                                        patience=3, min_lr=0.000001)
     cbs.append(lrr)
 
     x = K.layers.GlobalAveragePooling2D()(x)
