acc592707c2bda4a8cc4f70fa21d6a6c48dbfedc
mokasp <kasperbouldin@gmail.com>
2024-03-27 21:25:23 -0500

    almost final draft...

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -2,6 +2,9 @@
 import tensorflow.keras as K
 import tensorflow as tf
 
+import tensorflow.keras as K
+import tensorflow as tf
+
 def preprocess_data(X, Y):
     inp = K.applications.inception_resnet_v2.preprocess_input(X)
     labels = K.utils.to_categorical(Y, 10)
@@ -13,13 +16,15 @@ px_train, py_train = preprocess_data(x_train, y_train)
 px_test, py_test = preprocess_data(x_test, y_test)
 p_test = px_test, py_test
 
-data_generator = K.preprocessing.image.ImageDataGenerator(rotation_range=20,
+data_generator = K.preprocessing.image.ImageDataGenerator(rotation_range=40,
                    width_shift_range=0.2,
                    height_shift_range=0.2,
                    shear_range=0.2,
                    zoom_range=0.2,
-                   horizontal_flip=True)
+                   horizontal_flip=True,
+                   fill_mode='nearest')
 data_generator.fit(px_train)
+
 
 base_model = K.applications.InceptionResNetV2(include_top=False, weights='imagenet')
 base_model.trainable = False
@@ -28,12 +33,12 @@ scale = K.layers.Lambda(lambda x: tf.image.resize(x, (229, 229)))(inputs)
 x = base_model(scale, training=False)
 
 cbs = []
-save = K.callbacks.ModelCheckpoint('cifar10.h5', save_best_only=True, mode='max', monitor='val_accuracy')
+save = K.callbacks.ModelCheckpoint('cifar10.h5', monitor='val_accuracy', save_best_only=True, mode='max')
 cbs.append(save)
-e_s = K.callbacks.EarlyStopping(patience=7)
+e_s = K.callbacks.EarlyStopping(patience=6, restore_best_weights=True)
 cbs.append(e_s)
 lrr = K.callbacks.ReduceLROnPlateau(monitor='val_accuracy', factor=0.1,
-                              patience=3, min_lr=0.000000001)
+                              patience=3, min_lr=0.00000001)
 cbs.append(lrr)
 
 x = K.layers.GlobalAveragePooling2D()(x)
@@ -56,4 +61,4 @@ outputs = K.layers.Dense(10, activation='softmax')(x)
 
 model = K.Model(inputs, outputs)
 model.compile(optimizer=K.optimizers.Adam(), loss=K.losses.CategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-model.fit(x=data_generator.flow(px_train, y=py_train), epochs=50, batch_size=64, callbacks=cbs, shuffle=True, validation_data=p_test)
+model.fit(x=data_generator.flow(px_train, py_train), epochs=50, batch_size=64, callbacks=cbs, shuffle=True, validation_data=p_test)
