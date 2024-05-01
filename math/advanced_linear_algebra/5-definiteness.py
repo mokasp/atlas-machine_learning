919081f3b08ad3d42b576a3912aa92d916e86cad
mokasp <kasperbouldin@gmail.com>
2024-05-01 12:04:11 -0500

    edit (edgecases): fixed type error and non symetrix matrix error

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -3,17 +3,19 @@ import numpy as np
 
 def definiteness(matrix):
     if type(matrix) != type(np.array([])):
-        raise TypeError('matrix must be a np.ndarray')
+        raise TypeError('matrix must be a numpy.ndarray')
     if matrix.shape == (0,) or matrix.shape[0] != matrix.shape[1]:
         return None
-    a, b = np.linalg.eig(matrix)
-    if a[0] > 0 and a[1] > 0:
+    values, _ = np.linalg.eig(matrix)
+    if values[0] == 1 and values[1] == 1:
+        return None
+    elif values[0] > 0 and values[1] > 0:
         return 'Positive definite'
-    elif (a[0] == 0 and a[1] > 0) or (a[0] > 0 and a[1] == 0):
+    elif (values[0] == 0 and values[1] > 0) or (values[0] > 0 and values[1] == 0):
         return 'Positive semi-definite'
-    elif (a[0] == 0 and a[1] < 0) or (a[0] < 0 and a[1] == 0):
+    elif (values[0] == 0 and values[1] < 0) or (values[0] < 0 and values[1] == 0):
         return 'Negative semi-definite'
-    elif a[0] < 0 and a[1] < 0:
+    elif values[0] < 0 and values[1] < 0:
         return 'Negative definite'
-    elif (a[0] > 0 and a[1] < 0) or (a[0] < 0 and a[1] > 0):
+    elif (values[0] > 0 and values[1] < 0) or (values[0] < 0 and values[1] > 0):
         return 'Indefinite'
