846a32fe90f7601bac545ce702283804df47b752
mokasp <kasperbouldin@gmail.com>
2024-05-02 00:22:42 -0500

    add (func): added function that finds the mean and covariance matrix

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def mean_cov(X):
+    n = len(X)
+    d = len(X[0])
+
+    mean = []
+
+    cov = np.zeros((d, d))
+    for i in range(d):
+        s = 0
+        for j in range(n):
+            s += X[j][i]
+        mean.append(s / n)
+    
+    for i in range(d):
+        for j in range(i, d):
+            s = 0
+            for h in range(n):
+                s += (X[h][i] - mean[i]) * (X[h][j] - mean[j])
+                cov[i][j] = s / (n - 1)
+                cov[j][i] = cov[i][j]
+    return mean, cov
