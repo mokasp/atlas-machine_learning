c4c479dc571069d43ba679c45bda9607f985ea86
mokasp <kasperbouldin@gmail.com>
2024-05-05 13:32:37 -0500

    edit (style and doc): fixed pycodestyle and documentation

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
+""" tbw """
 import numpy as np
 
 
 def likelihood(x, n, P):
-    if type(n) is not int or n < 1:
+    """ tbw """
+    if not isinstance(n, int) or n < 1:
         raise ValueError('n must be a positive integer')
 
-    if type(x) is not int or x < 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
 
     if x > n:
         raise ValueError('x cannot be greater than n')
 
-    if type(P) != type(np.array([])) or len(P.shape) < 1 or P.shape[0] <= 1:
+    if not isinstance(P, type(np.array([]))) or len(
+            P.shape) < 1 or P.shape[0] <= 1:
         raise TypeError('P must be a 1D numpy.ndarray')
-
-
 
     temp = []
 
--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
+""" tbw """
 import numpy as np
 
 
 def intersection(x, n, P, Pr):
+    """ tbw """
 
-    if type(n) is not int or n < 1:
+    if not isinstance(n, int) or n < 1:
         raise ValueError('n must be a positive integer')
 
-    if type(x) is not int or x < 0:
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
 
     if x > n:
         raise ValueError('x cannot be greater than n')
 
-    if type(P) != type(np.array([])) or len(P.shape) < 1 or P.shape[0] <= 1:
+    if not isinstance(P, type(np.array([]))) or len(
+            P.shape) < 1 or P.shape[0] <= 1:
         raise TypeError('P must be a 1D numpy.ndarray')
 
-    if type(Pr) != type(np.array([])) or Pr.shape != P.shape:
+    if not isinstance(Pr, type(np.array([]))) or Pr.shape != P.shape:
         raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
-
 
     temp = []
 
@@ -29,7 +32,6 @@ def intersection(x, n, P, Pr):
 
         if Pr[i] > 1 or Pr[i] < 0:
             raise ValueError('All values in Pr must be in the range [0, 1]')
-
 
         n_f = 1
         for j in range(1, n + 1):
@@ -44,7 +46,7 @@ def intersection(x, n, P, Pr):
         Lp = coeff * (P[i] ** x) * ((1 - P[i]) ** (n - x))
         ints = Lp * Pr[i]
         temp.append(ints)
-    
+
     if not np.isclose(sum(Pr), 1):
         raise ValueError('Pr must sum to 1')
     return np.array(temp)
