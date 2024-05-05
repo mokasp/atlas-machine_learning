168ced98d36ae219f80b530421e78e14b08a8471
mokasp <kasperbouldin@gmail.com>
2024-05-05 03:12:29 -0500

    add (func): added basic function that finds intersection of hypothesis and evidence

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    temp = []
+    print(Pr)
+
+    for i in range(len(P)):
+        n_f = 1
+        for j in range(1, n + 1):
+            n_f *= j
+        x_f = 1
+        nx_f = 1
+        for j in range(1, x + 1):
+            x_f *= j
+        for j in range(1, (n - x) + 1):
+            nx_f *= j
+        coeff = n_f / (x_f * nx_f)
+        Lp = coeff * (P[i] ** x) * ((1 - P[i]) ** (n - x))
+        ints = Lp * Pr[i]
+        temp.append(ints)
+    return np.array(temp)
