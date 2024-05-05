59604ef7021124d15bc16dcc6a03d6238b7ef17a
mokasp <kasperbouldin@gmail.com>
2024-05-05 04:38:46 -0500

    add (edgecases): and checking for types and sizes of np arrays

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -4,6 +4,11 @@ import numpy as np
 
 def correlation(C):
 
+    if type(C) != type(np.array([])):
+        raise TypeError('C must be a numpy.ndarry')
+    if C.shape[0] != C.shape[1]:
+        raise ValueError('C must be a 2D square matrix')
+
     #extract square roots of values along the diagnal
     sqr_diag = np.sqrt(np.diag(C))
 
