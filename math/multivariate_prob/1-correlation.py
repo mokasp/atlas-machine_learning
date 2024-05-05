cb13c7c60ffaced7ae6ce48bec0039b4639be241
mokasp <kasperbouldin@gmail.com>
2024-05-05 05:26:01 -0500

    edit (style and doc): pycodestyle fix and temp documentation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
+""" tbw """
 import numpy as np
 
 
 def correlation(C):
+    """ tbw """
 
-    if type(C) != type(np.array([])):
+    if not isinstance(C, type(np.array([]))):
         raise TypeError('C must be a numpy.ndarray')
     if len(C.shape) < 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
 
-    #extract square roots of values along the diagnal
+    # extract square roots of values along the diagnal
     sqr_diag = np.sqrt(np.diag(C))
 
     out = np.outer(sqr_diag, sqr_diag)
-
 
     return C / out
