01de83f351e3d36382d22480af8358962c1dbee7
mokasp <kasperbouldin@gmail.com>
2024-05-05 05:51:37 -0500

    edit (edgecase): adding edgecases, typechecking and value checking!

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -3,9 +3,27 @@ import numpy as np
 
 
 def likelihood(x, n, P):
+    if type(n) is not int or n < 1:
+        raise ValueError('n must be a positive integer')
+
+    if type(x) is not int or n < 0:
+        raise ValueError('x must be an integer that is greater than or equal to 0')
+
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+
+    if type(P) != type(np.array([])) or len(P.shape) < 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+
+
+
     temp = []
 
     for i in range(len(P)):
+
+        if P[i] > 1 or P[i] < 0:
+            raise ValueError('All values in P must be in the range [0, 1]')
+
         n_f = 1
         for j in range(1, n + 1):
             n_f *= j
