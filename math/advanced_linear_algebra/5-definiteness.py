1dab50669526597c63602439021c0f56ea881fcc
mokasp <kasperbouldin@gmail.com>
2024-05-08 22:19:51 -0500

    edit: pycodestyle and documentation

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+""" module containing function that calculate the definiteness of a matrix."""
 import numpy as np
 
+
 def definiteness(matrix):
-    if type(matrix) != type(np.array([])):
+    """ function that calculate the definiteness of a matrix.
+
+    Args:
+        matrix (numpy.ndarray): a square matrix
+
+    Returns:
+        str or None: The definiteness of the matrix as a string
+    """
+    if not isinstance(matrix, type(np.array([]))):
         raise TypeError('matrix must be a numpy.ndarray')
     elif matrix.shape == (0,) or matrix.shape[0] != matrix.shape[1]:
         return None
@@ -11,11 +21,14 @@ def definiteness(matrix):
     values, _ = np.linalg.eig(matrix)
     if values[0] > 0 and values[1] > 0:
         return 'Positive definite'
-    elif (values[0] == 0 and values[1] > 0) or (values[0] > 0 and values[1] == 0):
+    elif (values[0] == 0 and values[1] > 0) or (values[0] > 0
+                                                and values[1] == 0):
         return 'Positive semi-definite'
-    elif (values[0] == 0 and values[1] < 0) or (values[0] < 0 and values[1] == 0):
+    elif (values[0] == 0 and values[1] < 0) or (values[0] < 0 and
+                                                values[1] == 0):
         return 'Negative semi-definite'
     elif values[0] < 0 and values[1] < 0:
         return 'Negative definite'
-    elif (values[0] > 0 and values[1] < 0) or (values[0] < 0 and values[1] > 0):
+    elif (values[0] > 0 and values[1] < 0) or (values[0] < 0 and
+                                               values[1] > 0):
         return 'Indefinite'
