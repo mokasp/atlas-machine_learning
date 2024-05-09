982b4629d177662e2653fed27ffdea777cddb479
mokasp <kasperbouldin@gmail.com>
2024-05-08 22:14:47 -0500

    edit: check for matrix symmetry

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -4,7 +4,9 @@ import numpy as np
 def definiteness(matrix):
     if type(matrix) != type(np.array([])):
         raise TypeError('matrix must be a numpy.ndarray')
-    if matrix.shape == (0,) or matrix.shape[0] != matrix.shape[1]:
+    elif matrix.shape == (0,) or matrix.shape[0] != matrix.shape[1]:
+        return None
+    elif matrix[0][1] != matrix[1][0]:
         return None
     values, _ = np.linalg.eig(matrix)
     if values[0] > 0 and values[1] > 0:
