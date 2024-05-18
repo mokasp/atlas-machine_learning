c77e2b17c9ebd337aa2add1efbfa23df91db464f
mokasp <kasperbouldin@gmail.com>
2024-05-18 12:59:05 -0500

    recompute labels before returning

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import numpy as np
 
+def get_centroid(X, centroids):
+    distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=-1)
+    return np.argmin(distances, axis=-1)
 
 def kmeans(X, k, iterations=1000):
     if type(X) != type(np.array([])) or len(X.shape) < 2 or type(X[0][0]) == type(np.array([])) :
@@ -16,21 +19,18 @@ def kmeans(X, k, iterations=1000):
 
     centroids = np.random.uniform(low=min_v, high=max_v, size=(k, d))
     for x in range(iterations):
-        distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=-1)
-        labels = np.argmin(distances, axis=-1)
+        labels = get_centroid(X, centroids)
 
         new_centroids = np.zeros((k, d))
 
-        for i in range(k):
-            if np.sum(labels == i) <= 0:
-                new_centroids[i] = np.random.uniform(low=min_v, high=max_v, size=d)
-            else:
-                new_centroids[i] = np.mean(X[labels == i], axis=0)
+        new_centroids = np.array([np.mean(X[labels == i], axis=0) if np.sum(labels == i) > 0 else np.random.uniform(min_v, labels, size=d) for i in range(k)])
 
         if np.allclose(new_centroids, centroids):
+            labels = get_centroid(X, centroids)
             return centroids, labels
         
         centroids = new_centroids
-
+        
+    labels = get_centroid(X, centroids)
 
     return centroids, labels
