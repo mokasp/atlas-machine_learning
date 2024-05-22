20b547738d459ec948a6e983f1edd5c500914df4
mokasp <kasperbouldin@gmail.com>
2024-05-22 14:47:07 -0500

    checking param validity, comments, documentation, and pycodestyle

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
+""" module containing function that tests for the optimum number of
+    clusters by variance."""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    if kmax == None:
+    """ function that tests for the optimum number of clusters by variance.
+
+    Parameters
+    ----------
+    X : numpy.ndarray
+        Dataset with shape (n, d).
+    kmin : int, optional
+        Minimum number of clusters to check for (inclusive). Default is 1.
+    kmax : int or None, optional
+        Maximum number of clusters to check for (inclusive). Default is None.
+    iterations : int, optional
+        Maximum number of iterations for K-means. Default is 1000.
+    """
+
+    # check valid X
+    if not isinstance(X, type(np.array([]))) or len(
+            X.shape) < 2 or isinstance(X[0][0], type(np.array([]))):
+        return None, None
+
+    # check validity of kmin and kmax
+    if not isinstance(kmin, int) or kmin <= 0 or not isinstance(
+            kmax, int) or kmax <= 0:
+        return None, None
+
+    # check validity of iterations
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+
+    if kmax is None:
         kmax = X.shape[0]
     results, d_vars = [], []
     for i in range(kmin, kmax + 1):
