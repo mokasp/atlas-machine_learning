c39760e5f95137f8834b3811ac44c1f99f91822a
mokasp <kasperbouldin@gmail.com>
2024-05-22 11:46:56 -0500

    pyycodestyle and documentation

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
+""" module containing function that initialize cluster centroids for
+K-means clustering. """
 import numpy as np
 
 
 def initialize(X, k):
+    """ function that initialize cluster centroids for K-means clustering.
 
-    if type(X) != type(np.array([])) or len(X.shape) < 2 or type(X[0][0]) == type(np.array([])) :
+    Parameters
+    ----------
+    X : numpy.ndarray
+        Dataset with shape (n, d).
+    k : int
+        Number of clusters.
+
+    Returns
+    -------
+    centroids : numpy.ndarray or None
+        Initialized centroids for each cluster with shape (k, d).
+        Returns None on failure."""
+
+    if type(X) is not type(np.array([])) or len(X.shape) < 2 or type(
+            X[0][0]) is type(np.array([])):
         return None
-    
-    if type(k) != int or k <= 0:
+
+    if type(k) is not int or k <= 0:
         return None
 
     d = X.shape[1]
 
     min_v = np.min(X, axis=0)
     max_v = np.max(X, axis=0)
+    print(min_v)
+    print(max_v)
 
     return np.random.uniform(low=min_v, high=max_v, size=(k, d))
