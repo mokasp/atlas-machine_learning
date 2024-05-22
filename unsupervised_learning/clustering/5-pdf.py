e8d497bbf3e55c05c8c7b444ab28435c4b1d5d55
mokasp <kasperbouldin@gmail.com>
2024-05-22 18:46:51 -0500

    param validation

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -30,7 +30,10 @@ def pdf(X, m, S):
 
     # check validity of m
     if not isinstance(m, type(np.array([]))) or len(
-            m.shape) > 1 or not m[0].is_integer():
+            m.shape) > 1:
+        return None
+    
+    if not isinstance(m[0], np.int64) or not m[0].is_integer():
         return None
 
     # check validity of S
