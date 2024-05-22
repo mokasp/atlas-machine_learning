10e65a986966fa205731e1122bfd1b94dd09ae07
mokasp <kasperbouldin@gmail.com>
2024-05-22 15:28:29 -0500

    added comments, documentation, and pycodestyle

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,35 +1,66 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3\
+""" module containing function that calculates the probability density
+function of a Gaussian distribution. """
 import numpy as np
 
 
-def pdf(X,m, S):
+def pdf(X, m, S):
+    """ function that calculates the probability density function of a
+        Gaussian distribution.
+
+        Parameters
+        ----------
+        X : numpy.ndarray
+            Data points with shape (n, d).
+        m : numpy.ndarray
+            Mean of the distribution with shape (d,).
+        S : numpy.ndarray
+            Covariance of the distribution with shape (d, d).
+
+        Returns
+        -------
+        P : numpy.ndarray or None
+            PDF values for each data point with shape (n,).
+            Returns None on failure.
+    """
+
+    # check validity of X
+    if not isinstance(X, type(np.array([]))) or len(
+            X.shape) < 2 or isinstance(X[0][0], type(np.array([]))):
+        return None, None
+
+    # get shapes
     k = len(m)
 
+    # reshape mean to make suitable in computation
     mm = m.reshape(k, 1)
 
-    # constant
+    # find constant
     const = (2 * np.pi) ** k
 
-    # determinate of covariance mat
+    # determinate and inverse of covariance mat
     det = np.linalg.det(S)
     inv = np.linalg.inv(S)
 
     # scaling
     scale = 1.0 / np.sqrt(const * det)
 
+    # mean shifted X
     diff = (X - mm.T)
 
+    # calc exponential term
     exp_term = -0.5 * (np.dot(diff, inv).dot(diff.T))
 
+    # create mask to extract diagonal values
     row, col = exp_term.shape
     diag = np.eye(row, col, dtype=bool)
-
     exp_diag = exp_term[diag]
 
+    # combine
     res = np.log(scale) + exp_diag
-
     p_d_f = np.exp(res)
 
+    # set min value to 1e-300
     p_d_f[p_d_f < 1e-300] = 1e-300
 
     return p_d_f
