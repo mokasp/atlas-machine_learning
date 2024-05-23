3c9c83afe3812cbcd9837329edd9af81cba099d0
mokasp <kasperbouldin@gmail.com>
2024-05-22 19:09:16 -0500

    edit param validation

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -35,8 +35,7 @@ def maximization(X, g):
 
     # check validity of g
     if not isinstance(g, type(np.array([]))) or \
-        len(g.shape) < 2 or g.shape[1] != X.shape[0] or \
-            not np.isclose(np.sum(g[0]), 1):
+        len(g.shape) < 2 or g.shape[1] != X.shape[0]:
         return None, None, None
 
     # get shapes
