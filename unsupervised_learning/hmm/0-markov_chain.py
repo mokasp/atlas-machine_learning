093e699488f8880ac8cf107ea3dc15e9e4e10993
mokasp <kasperbouldin@gmail.com>
2024-05-25 19:37:32 -0500

    testing out markov chain

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+import numpy as np
+
+
+def markov_chain(P, s, t=1):
+    n = len(s[0])
+    states = np.arange(n)
+    state = np.random.choice(states, p=s[0])
+
+    dist = []
+
+    for i in range(t):
+        current = state
+
+        p_dist = P[list(states).index(current)]
+        
+        dist.append(p_dist)
+        state = np.random.choice(states, p=p_dist)
+    return np.array(np.mean(dist, axis=0))
