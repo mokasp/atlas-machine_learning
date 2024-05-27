4401b016baed34db3c74aa9db64e25ec99c206d5
mokasp <kasperbouldin@gmail.com>
2024-05-26 22:41:03 -0500

    using eigenvectors and values to find steady-state probability

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+import numpy as np
+
+
+def regular(p):
+         
+        # check to make sure all values in matrix are positive
+        if p.all() > 0:
+
+            # get the eigen values and eigen vectors
+            values, vectors = np.linalg.eig(p.T)
+
+            # find the index of the eigenvalue that equals 1
+            idx = list(np.round(values, decimals=1)).index(1)
+
+            # get the vector associated with the value of 1
+            sstate_prob = vectors[:, idx]
+
+            # normalize so probabilities add to 1
+            sstate_prob /= sum(sstate_prob)
+
+            return  sstate_prob
+        return None
