93229549e321aef1c7ba56876e917623383cb0a6
mokasp <kasperbouldin@gmail.com>
2024-05-27 10:47:35 -0500

    documentation

--- unsupervised_learning/hmm/5-backward.py
+++ unsupervised_learning/hmm/5-backward.py
@@ -1,9 +1,40 @@
 #!/usr/bin/env python3
+""" module containing function that performs the b0ackward algorithm for a
+    Hidden Markov Model. """
 import numpy as np
 
 
 def backward(Observation, Emission, Transition, Initial):
+    """ function that performs the backward algorithm for a
+        Hidden Markov Model.
 
+        Parameters
+        ----------
+        Observation : numpy.ndarray
+            Array of shape (T,) containing the index of the observation.
+        Emission : numpy.ndarray
+            Array of shape (N, M) containing emission probabilities.
+            Emission[i, j] is the probability of observing j given the
+            hidden state i.
+        Transition : numpy.ndarray
+            Array of shape (N, N) containing transition probabilities.
+            Transition[i, j] is the probability of transitioning from
+            hidden state i to j.
+        Initial : numpy.ndarray
+            Array of shape (N, 1) containing the probability of starting
+            in a particular hidden state.
+
+        Returns
+        -------
+        P : float or None
+            Likelihood of the observations given the model.
+            None on failure.
+        B : numpy.ndarray or None
+            Array of shape (N, T) containing backward path probabilities.
+            B[i, j] is the probability of generating the future observations
+            from hidden state i at time j.
+            None on failure.
+    """
     # reshape initial dist to make compatible with other matrices
     init = Initial.T[0]
 
