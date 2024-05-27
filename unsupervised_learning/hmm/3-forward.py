ecb288adae14b0c93889c57f9f48d035fb5435e0
mokasp <kasperbouldin@gmail.com>
2024-05-27 10:41:21 -0500

    documentation

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
+""" module containing function that performs the forward algorithm for a
+    Hidden Markov Model."""
 import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
+    """ function that performs the forward algorithm for a Hidden Markov
+        Model.
+
+        Parameters
+        ----------
+        Observation : numpy.ndarray
+            Array of shape (T,) containing the index of the observation.
+        Emission : numpy.ndarray
+            Array of shape (N, M) containing emission probabilities.
+            Emission[i, j] is the probability of observing j given the
+            hidden state i.
+        Transition : numpy.ndarray
+            Array of shape (N, N) containing transition probabilities.
+            Transition[i, j] is the probability of transitioning from
+            hidden state i to j.
+        Initial : numpy.ndarray
+            Array of shape (N, 1) containing the probability of starting
+            in a particular hidden state.
+
+        Returns
+        -------
+        P : float or None
+            Likelihood of the observations given the model.
+        F : numpy.ndarray or None
+            Array of shape (N, T) containing forward path probabilities.
+            F[i, j] is the probability of being in hidden state i at
+            time j given the previous observations.
+            None on failure.
+    """
 
     # reshape initial dist to make compatible with other matrices
     init = Initial.T[0]
