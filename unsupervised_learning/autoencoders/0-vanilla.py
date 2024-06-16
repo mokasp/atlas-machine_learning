9942e53f77a27a58d7fb7862b6eb6c8aa156579b
mokasp <kasperbouldin@gmail.com>
2024-06-16 15:02:38 -0500

    (change) specifiy units param for layers

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -6,16 +6,16 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     # encoder
     encode_inpt = K.layers.Input(shape=(input_dims,))
-    first_hidden = K.layers.Dense(hidden_layers[0], activation='relu')(encode_inpt)
-    second_hidden = K.layers.Dense(hidden_layers[1], activation='relu')(first_hidden)
-    latent = K.layers.Dense(latent_dims, activation='relu')(second_hidden)
+    first_hidden = K.layers.Dense(units=hidden_layers[0], activation='relu')(encode_inpt)
+    second_hidden = K.layers.Dense(units=hidden_layers[1], activation='relu')(first_hidden)
+    latent = K.layers.Dense(units=latent_dims, activation='relu')(second_hidden)
     encode = K.Model(inputs=inpt, outputs=latent)
 
     # decoder
     inpt = K.layers.Input(shape=(latent_dims,))
-    first_hidden = K.layers.Dense(hidden_layers[1], activation='relu')(inpt)
-    second_hidden = K.layers.Dense(hidden_layers[0], activation='relu')(first_hidden)
-    latent = K.layers.Dense(input_dims, activation='sigmoid')(second_hidden)
+    first_hidden = K.layers.Dense(units=hidden_layers[1], activation='relu')(inpt)
+    second_hidden = K.layers.Dense(units=hidden_layers[0], activation='relu')(first_hidden)
+    latent = K.layers.Dense(units=input_dims, activation='sigmoid')(second_hidden)
     decode = K.Model(inputs=inpt, outputs=latent)
 
     # full autoencoder
