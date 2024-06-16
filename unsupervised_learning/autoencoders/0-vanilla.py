8d984a6bf095047238072bf78f8365f28b806aa6
mokasp <kasperbouldin@gmail.com>
2024-06-16 15:20:54 -0500

    pycodestyle and documentation

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
+""" module containing function that contructs a vanilla autoencodert """
 import tensorflow.keras as K
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ function that contructs a vanilla autoencoder
+
+        Parameters
+        ----------
+        input_dims : int
+            Dimensions of the model input.
+        hidden_layers : list
+            List containing the number of nodes for each hidden layer in
+            the encoder.
+        latent_dims : int
+            Dimensions of the latent space representation.
+
+        Returns
+        -------
+        encoder : tensorflow.keras.Model
+            The encoder model.
+        decoder : tensorflow.keras.Model
+            The decoder model.
+        autoencoder : tensorflow.keras.Model
+            The full autoencoder model.
+    """
 
     layers = len(hidden_layers)
     decode_layers = hidden_layers[::-1]
@@ -11,7 +33,8 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     encode_inpt = K.layers.Input(shape=(input_dims,))
     layer = encode_inpt
     for i in range(layers):
-        layer = K.layers.Dense(units=hidden_layers[i], activation='relu')(layer)
+        layer = K.layers.Dense(units=hidden_layers[i],
+                               activation='relu')(layer)
     latent = K.layers.Dense(units=latent_dims, activation='relu')(layer)
     encode = K.Model(inputs=encode_inpt, outputs=latent)
 
@@ -19,12 +42,14 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     inpt = K.layers.Input(shape=(latent_dims,))
     layer = inpt
     for i in range(layers):
-        layer = K.layers.Dense(units=decode_layers[i], activation='relu')(layer)
+        layer = K.layers.Dense(units=decode_layers[i],
+                               activation='relu')(layer)
     latent = K.layers.Dense(units=input_dims, activation='sigmoid')(layer)
     decode = K.Model(inputs=inpt, outputs=latent)
 
     # full autoencoder
-    autoencode = K.Model(inputs=encode_inpt, outputs=decode(encode(encode_inpt)))
+    autoencode = K.Model(inputs=encode_inpt, outputs=decode(encode
+                                                            (encode_inpt)))
 
     # compile
     autoencode.compile(optimizer='adam', loss='binary_crossentropy')
