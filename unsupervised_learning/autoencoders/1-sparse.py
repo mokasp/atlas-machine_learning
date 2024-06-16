3d8b85e87e6f5c46cfe3b06f59150fbacb1fc87d
mokasp <kasperbouldin@gmail.com>
2024-06-16 16:09:55 -0500

    pycodestyle and documentation

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python3
+""" module containing a function that contructs a sparse autoencoder """
 import tensorflow.keras as K
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """ function that contructs a sparse autoencoder
 
+        Parameters
+        ----------
+        input_dims : int
+            Dimensions of the model input.
+        hidden_layers : list
+            List containing the number of nodes for each hidden layer in
+            the encoder.
+        latent_dims : int
+            Dimensions of the latent space representation.
+        lambtha : float
+            Regularization parameter for L1 regularization
+
+        Returns
+        -------
+        encoder : tensorflow.keras.Model
+            The encoder model.
+        decoder : tensorflow.keras.Model
+            The decoder model.
+        autoencoder : tensorflow.keras.Model
+            The full autoencoder model.
+    """
 
     layers = len(hidden_layers)
     decode_layers = hidden_layers[::-1]
@@ -14,7 +37,9 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     for i in range(layers):
         layer = K.layers.Dense(units=hidden_layers[i],
                                activation='relu')(layer)
-    latent = K.layers.Dense(units=latent_dims, activation='relu', activity_regularizer=K.regularizers.L1(lambtha))(layer)
+    latent = K.layers.Dense(units=latent_dims, activation='relu',
+                            activity_regularizer=K.regularizers.L1(lambtha)
+                            )(layer)
     encode = K.Model(inputs=encode_inpt, outputs=latent)
 
     # decoder
