ee3d88c05b9dc941bd6a607803cfbb764a1b1381
mokasp <kasperbouldin@gmail.com>
2024-06-16 14:39:32 -0500

    (change) import tensorflow as tf -> import tensorflow.keras as K

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-import numpy as np
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
 
     # encoder
-    inpt = tf.keras.layers.Input(shape=input_dims)
-    first_hidden = tf.keras.layers.Dense(hidden_layers[0], activation='relu')(inpt)
-    second_hidden = tf.keras.layers.Dense(hidden_layers[1], activation='relu')(first_hidden)
-    latent = tf.keras.layers.Dense(latent_dims, activation='relu')(second_hidden)
-    encode = tf.keras.Model(inputs=inpt, outputs=latent)
+    inpt = K.layers.Input(shape=input_dims)
+    first_hidden = K.layers.Dense(hidden_layers[0], activation='relu')(inpt)
+    second_hidden = K.layers.Dense(hidden_layers[1], activation='relu')(first_hidden)
+    latent = K.layers.Dense(latent_dims, activation='relu')(second_hidden)
+    encode = K.Model(inputs=inpt, outputs=latent)
 
     # decoder
-    inpt = tf.keras.layers.Input(shape=latent_dims)
-    first_hidden = tf.keras.layers.Dense(hidden_layers[1], activation='relu')(inpt)
-    second_hidden = tf.keras.layers.Dense(hidden_layers[0], activation='relu')(first_hidden)
-    latent = tf.keras.layers.Dense(input_dims, activation='sigmoid')(second_hidden)
-    decode = tf.keras.Model(inputs=inpt, outputs=latent)
+    inpt = K.layers.Input(shape=latent_dims)
+    first_hidden = K.layers.Dense(hidden_layers[1], activation='relu')(inpt)
+    second_hidden = K.layers.Dense(hidden_layers[0], activation='relu')(first_hidden)
+    latent = K.layers.Dense(input_dims, activation='sigmoid')(second_hidden)
+    decode = K.Model(inputs=inpt, outputs=latent)
 
     # full autoencoder
-    autoencode = tf.keras.Model(inputs=encode.input, outputs=decode(encode.output))
+    autoencode = K.Model(inputs=encode.input, outputs=decode(encode.output))
 
     # compile
-    autoencode.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy())
+    autoencode.compile(optimizer='adam', loss=K.losses.BinaryCrossentropy())
 
     return encode, decode, autoencode
