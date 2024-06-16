9c3114f81265f53ae5233f40f0b14cc24b0cee0d
mokasp <kasperbouldin@gmail.com>
2024-06-16 16:06:35 -0500

    inital architecture for sparse autoencoder

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+
+
+    layers = len(hidden_layers)
+    decode_layers = hidden_layers[::-1]
+
+    # encoder
+    encode_inpt = K.layers.Input(shape=(input_dims,))
+    layer = encode_inpt
+    for i in range(layers):
+        layer = K.layers.Dense(units=hidden_layers[i],
+                               activation='relu')(layer)
+    latent = K.layers.Dense(units=latent_dims, activation='relu', activity_regularizer=K.regularizers.L1(lambtha))(layer)
+    encode = K.Model(inputs=encode_inpt, outputs=latent)
+
+    # decoder
+    inpt = K.layers.Input(shape=(latent_dims,))
+    layer = inpt
+    for i in range(layers):
+        layer = K.layers.Dense(units=decode_layers[i],
+                               activation='relu')(layer)
+    latent = K.layers.Dense(units=input_dims, activation='sigmoid')(layer)
+    decode = K.Model(inputs=inpt, outputs=latent)
+
+    # full autoencoder
+    autoencode = K.Model(inputs=encode_inpt, outputs=decode(encode
+                                                            (encode_inpt)))
+
+    # compile
+    autoencode.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encode, decode, autoencode
