e7e01f7415e872479108eb3c8ec9bb248f3b02c7
mokasp <kasperbouldin@gmail.com>
2024-06-26 21:54:53 -0500

    not working locally, not sure if its possible

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import tensorflow.keras as K
+import tensorflow as tf
 
 
 def autoencoder(input_dims, filters, latent_dims):
@@ -27,8 +28,8 @@ def autoencoder(input_dims, filters, latent_dims):
 
     def sample(params):
         z_mean, z_logsig = params
-        eps = K.random_normal(shape=(K.shape(z_mean)[0], latent_dims), mean=0.0, stddev=0.1)
-        out = z_mean + K.exp(z_logsig)
+        eps = K.backend.random_normal(shape=(K.backend.shape(z_mean)[0], K.backend.int_shape(z_mean)[1]), mean=0.0, stddev=0.1)
+        out = z_mean + K.backend.exp(z_logsig)
 
         return out * eps
 
@@ -43,7 +44,7 @@ def autoencoder(input_dims, filters, latent_dims):
                                activation='relu')(layer)
     z_mean = K.layers.Dense(units=latent_dims)(layer)
     z_logsig = K.layers.Dense(units=latent_dims)(layer)
-    points = layers.Lambda(sample)([z_mean, z_logsig])
+    points = K.layers.Lambda(sample, output_shape=(latent_dims,))([z_mean, z_logsig])
     encode = K.Model(inputs=encode_inpt, outputs=[z_mean, z_logsig, points])
 
     # decoder
@@ -55,14 +56,15 @@ def autoencoder(input_dims, filters, latent_dims):
     latent = K.layers.Dense(units=input_dims, activation='sigmoid')(layer)
     decode = K.Model(inputs=inpt, outputs=latent)
 
+
     # full autoencoder
     autoencode = K.Model(inputs=encode_inpt, outputs=decode(encode
                                                             (encode_inpt)[2]))
 
-    r_loss = K.losses.binary_crossentropy(encode_inpt, latent) * input_dims
-    kl_div = K.sum(1 + z_logsig - K.square(z_mean) - K.exp(z_logsig), axis=-1) * 0.5
-    total_loss = K.mean(r_loss + kl_div)
 
+    r_loss = K.losses.binary_crossentropy(encode_inpt, latent) * input_dims
+    kl_div = K.backend.sum(1 + z_logsig - K.backend.square(z_mean) - K.backend.exp(z_logsig), axis=-1) * -0.5
+    total_loss = K.backend.mean(r_loss + kl_div)
 
 
     # compile
