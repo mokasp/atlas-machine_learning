c5ca2b6741977fae95b4e2e01a5a309c7cb91ca6
mokasp <kasperbouldin@gmail.com>
2024-06-26 22:42:04 -0500

    pycode style

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+""" tbw """
 import tensorflow.keras as K
 
 
 def autoencoder(input_dims, filters, latent_dims):
+    """ tbw """
     layers = len(filters)
     decode_layers = filters[::-1]
 
@@ -10,7 +12,8 @@ def autoencoder(input_dims, filters, latent_dims):
     encode_inpt = K.layers.Input(shape=input_dims)
     layer = encode_inpt
     for i in range(layers):
-        layer = K.layers.Conv2D(filters[i], (3, 3), padding='same', activation='relu')(layer)
+        layer = K.layers.Conv2D(
+            filters[i], (3, 3), padding='same', activation='relu')(layer)
         layer = K.layers.MaxPooling2D((2, 2), padding='same')(layer)
     latent = layer
     encode = K.Model(inputs=encode_inpt, outputs=latent)
@@ -20,12 +23,17 @@ def autoencoder(input_dims, filters, latent_dims):
     layer = inpt
     for i in range(layers):
         if i != layers - 1:
-            layer = K.layers.Conv2D(decode_layers[i], (3, 3), padding='same', activation='relu')(layer)
+            layer = K.layers.Conv2D(
+                decode_layers[i], (3, 3), padding='same',
+                activation='relu')(layer)
             layer = K.layers.UpSampling2D((2, 2))(layer)
         else:
-            layer = K.layers.Conv2D(decode_layers[i], (3, 3), padding='valid', activation='relu')(layer)
+            layer = K.layers.Conv2D(
+                decode_layers[i], (3, 3), padding='valid',
+                activation='relu')(layer)
             layer = K.layers.UpSampling2D((2, 2))(layer)
-    latent = K.layers.Conv2D(input_dims[2], (3, 3), activation='sigmoid', padding='same')(layer)
+    latent = K.layers.Conv2D(
+        input_dims[2], (3, 3), activation='sigmoid', padding='same')(layer)
     decode = K.Model(inputs=inpt, outputs=latent)
 
     # full autoencoder
