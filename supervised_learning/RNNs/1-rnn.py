cf06dfbe3f05a57d2b56e6b866b6791f441e14c1
mokasp <kasperbouldin@gmail.com>
2024-07-03 03:26:46 -0500

    inital commit for full forward pass

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-""" module containing function that per"""
+import numpy as np
+""" module containing function that performs full forward propagation
+    with an RNN """
+
+
+def rnn(rnn_cell, X, h_0):
+    """ function that performs full forward propagation of an RNN """
+    h_prev = h_0
+    H = []
+    Y = []
+    H.append(h_prev)
+    for i in range(X.shape[0]):
+        h_prev, y = rnn_cell.forward(h_prev, X[i])
+        H.append(h_prev)
+        Y.append(y)
+    
+    return np.array(H), np.array(Y)
