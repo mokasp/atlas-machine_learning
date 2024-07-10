bded7d824597fc1ac6620d9cddaf06a09b2033aa
mokasp <kasperbouldin@gmail.com>
2024-07-10 03:34:04 -0500

    temp code to check weights

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -20,19 +20,10 @@ class LSTMCell():
         self.Wy = np.random.randn(h, o)
         self.by = np.zeros((1, o))
     
-    def forward(self, h_prev, x_t):
+    def forward(self, h_prev, c_prev, x_t):
         """ forward pass """
-        h = np.concatenate((h_prev, x_t), axis=1)
-
-        z_t = self.sigmoid(np.dot(h, self.Wz) + self.bz)
-        r_t = self.sigmoid(np.dot(h, self.Wr) + self.br)
-        
-        h_tt = np.concatenate((r_t * h_prev, x_t), axis=1)
-        h_hat_t = np.tanh(np.dot(h_tt, self.Wh) + self.bh)
-        h_t = (1 - z_t) * h_prev + z_t * h_hat_t
-        y = self.softmax(np.dot(h_t, self.Wy) + self.by)
-
-        return h_t, y
+        temp = np.ndarray([])
+        return temp, temp, temp
     
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
