71a93aac29e987e7b375384f1cc78c7572186b87
mokasp <kasperbouldin@gmail.com>
2024-07-25 10:22:52 -0500

    pycodestyle fix

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""" module containing function that convers a gensim word2vec model to a keras embedding layer """
+"""" module containing function that convers a gensim word2vec model to a
+    keras embedding layer """
 import gensim as gm
 import tensorflow.keras as K
 import numpy as np
 
 
 def gensim_to_keras(model):
-    """ function that converts a gensim word2vec model to a keras embedding layer """
+    """ function that converts a gensim word2vec model to a keras
+        embedding layer """
     sample = model.wv.index_to_key[0]
     rows = len(model.wv.index_to_key)
     cols = len(model.wv[sample])
@@ -16,5 +18,6 @@ def gensim_to_keras(model):
         embedding_mat[i] = model.wv[word]
         i += 1
 
-    embedding_layer = K.layers.Embedding(rows, cols, weights=[embedding_mat], trainable=True)
+    embedding_layer = K.layers.Embedding(
+        rows, cols, weights=[embedding_mat], trainable=True)
     return embedding_layer
--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -3,13 +3,25 @@
 import gensim as gm
 
 
-def fasttext_model(sentences, size=100, min_count=5, negative=5, window=5, cbow=True, iterations=5, seed=0, workers=1):
+def fasttext_model(sentences, size=100, min_count=5, negative=5,
+                   window=5, cbow=True, iterations=5, seed=0, workers=1):
     """ function that creates and trains a FastText model """
     if cbow:
         sg = 0
     else:
         sg = 1
-    fasttext = gm.models.FastText(vector_size=size, min_count=min_count, window=window, negative=negative, sg=sg, seed=seed, workers=workers, epochs=iterations)
+    fasttext = gm.models.FastText(
+        vector_size=size,
+        min_count=min_count,
+        window=window,
+        negative=negative,
+        sg=sg,
+        seed=seed,
+        workers=workers,
+        epochs=iterations)
     fasttext.build_vocab(sentences)
-    fasttext.train(sentences, total_examples=len(sentences), epochs=fasttext.epochs)
+    fasttext.train(
+        sentences,
+        total_examples=len(sentences),
+        epochs=fasttext.epochs)
     return fasttext
