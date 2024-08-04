ac40e34779fc57206a68e7f9e5486e617da9c49b
mokasp <kasperbouldin@gmail.com>
2024-08-04 04:07:06 -0500

    (add) dataset loader and processor class

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset():
+  def __init__(self):
+    data_train, data_valid = self.load_dataset()
+    self.data_train = data_train
+    self.data_valid = data_valid
+    tokenizer_en, tokenizer_pt = self.tokenize_dataset()
+    self.tokenizer_en = tokenizer_en
+    self.tokenizer_pt = tokenizer_pt
+  
+  def load_dataset(self):
+    pt2en_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+    pt2en_val = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+    return pt2en_train, pt2en_val
+  
+  def tokenize_dataset(self):
+    tokenize_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        (en.numpy() for _, en in self.data_train), target_vocab_size=2**15)
+    tokenize_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        (pt.numpy() for pt, _ in self.data_train), target_vocab_size=2**15)
+    return tokenize_en, tokenize_pt
