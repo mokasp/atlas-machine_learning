d75a8fcd03966eb1fd624badb0601116bbe010f7
mokasp <kasperbouldin@gmail.com>
2024-08-16 13:25:45 -0500

    (add) comments

--- supervised_learning/qa_bot/1-loop.py
+++ supervised_learning/qa_bot/1-loop.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+# begin question-answer loop
 running = True
 while running:
     closing = ['exit', 'quit', 'goodbye', 'bye']
+
+    # get users query
     question = input('Q: ').lower()
+      
+    # check if user wants to exit program
     if question in closing:
         print('A: Goodbye')
         running = False
     else:
-        print('A: ')
+        # if not, return blank answer
+        print(f'A: ')
