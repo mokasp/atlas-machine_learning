541c449c0be3f7852f32368d2e48402296dfe23b
mokasp <kasperbouldin@gmail.com>
2024-08-16 13:04:36 -0500

    (add) comments

--- supervised_learning/qa_bot/3-semantic_search.py
+++ supervised_learning/qa_bot/3-semantic_search.py
@@ -8,15 +8,20 @@ import os
 
 def semantic_search(corpus_path, sentence):
     
+    # collect all reference documents
     references = []
-    references.append(sentence)
     for file in os.listdir(corpus_path):
         if not file.startswith('.'):
             with open(corpus_path + '/' + file) as f:
                 references.append(f.read())
     
+    # load the sentence encoder
     sentence_encoder = th.load("https://tfhub.dev/google/universal-sentence-encoder-large/4")
+    # combine the query with the references and encode this vector
+    references = [sentence] + references
     outputs = sentence_encoder(references)['outputs']
+    
+    # find similarity score of each document to get most similar doc
     similarity_score = np.inner(outputs, outputs)[0][1:]
     similar_idx = np.argmax(similarity_score) + 1
     similar_doc = references[similar_idx]
