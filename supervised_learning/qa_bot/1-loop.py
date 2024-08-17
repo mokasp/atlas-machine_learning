76cf15da97f370cc728a12d008cb9ec8fe9445ce
mokasp <kasperbouldin@gmail.com>
2024-08-16 19:36:55 -0500

    (add & fix) documentation and pycodestyle

--- supervised_learning/qa_bot/1-loop.py
+++ supervised_learning/qa_bot/1-loop.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+""" this script initiates an interactive question-answering loop. Users can
+    input questions and receive answers. The loop continues until the user
+    decides to exit the program by typing a termination command.
+
+    Usage:
+        1. Run the script to start the interactive loop.
+        2. Enter questions to receive responses.
+        3. To exit the loop, type one of the termination commands such as
+           'exit', 'quit', 'goodbye', or 'bye'.
+
+    Functionality:
+        - Handles user input and provides a response based on the input.
+        - Continues to prompt the user for input until a termination command
+          is given.
+
+    Termination Commands:
+        - 'exit'
+        - 'quit'
+        - 'goodbye'
+        - 'bye'
+
+    Example:
+        Q: What is the capital of France?
+        A:
+
+        Q: exit
+        A: Goodbye
+"""
 
 # begin question-answer loop
 running = True
@@ -7,7 +35,7 @@ while running:
 
     # get users query
     question = input('Q: ').lower()
-      
+
     # check if user wants to exit program
     if question in closing:
         print('A: Goodbye')
