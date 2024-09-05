6993313681587ac3c1a8b2ac0d9ea8cd63c95d21
mokasp <kasperbouldin@gmail.com>
2024-09-04 21:44:26 -0500

    (add) documentation

--- reinforcement_learning/deep_q_learning/play.py
+++ reinforcement_learning/deep_q_learning/play.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+""" script that allows the trained reinforcement learning agent to play an episode
+    of Atari's Breakout and displays the game play.
+
+    Dependencies:
+        - gym==0.25.0: Standard API for reinforcement learning with a diverse
+        collection of reference environments.
+        - h5py==3.10.0: Python interface to the HDF5 binary data format, used for
+        managing large datasets.
+        - keras==2.13.1: High-level neural networks API, written in Python and capable
+        of running on top of TensorFlow, CNTK, or Theano.
+        - keras-rl2==1.0.4: Reinforcement learning library for Keras, providing
+        implementations of RL algorithms such as DQN, DDPG, and more.
+        - Pillow==10.0.1: Python Imaging Library (PIL) fork, which adds image processing
+        capabilities to Python.
+        - tensorflow==2.13.0: Open-source platform for machine learning with a comprehensive
+        ecosystem of tools, libraries, and community resources.
+        - numpy==1.23.5: Library for numerical operations in Python, providing support for
+        large, multi-dimensional arrays and matrices, along with mathematical
+        functions to operate on these arrays. 
+"""
 import gym
 from gym.wrappers import GrayScaleObservation, FrameStack, ResizeObservation
 import tensorflow.keras as K
