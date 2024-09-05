c70c406c06aaea03b8da82bb79859536dded08a0
mokasp <kasperbouldin@gmail.com>
2024-09-04 21:37:22 -0500

    (add) comments

--- reinforcement_learning/deep_q_learning/train.py
+++ reinforcement_learning/deep_q_learning/train.py
@@ -12,12 +12,19 @@ from rl.agents.dqn import DQNAgent
 from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 
+# load in breakout environment
 env = gym.make('BreakoutDeterministic-v4')
 
+# set up memory for the agent to store its past interactions
 memory = SequentialMemory(limit=1000000, window_length=4)
 
+# use the epsilon greedy policy
 policy = EpsGreedyQPolicy()
+
+# preprocess data by resizing to 84 x 84, grayscaling, and stacking into 4 frames
 env = FrameStack(GrayScaleObservation(ResizeObservation(env, shape=(84, 84))), 4)
+
+# build model according to the atari breakout paper
 actions = env.action_space.n
 model = Sequential([
           Input(shape=(84,84,4,)),
@@ -28,9 +35,15 @@ model = Sequential([
           Dense(units=256, activation="relu"),
           Dense(units=actions, activation="linear"),
       ])
+
+# initialize the agent itself
 dqn = DQNAgent(model=model, memory=memory, policy=policy,
                   nb_actions=env.action_space.n, nb_steps_warmup=10000,
                   target_model_update=1e-2, gamma=.99)
+
+# compile the agent with the adam optimizer
 dqn.compile(Adam(learning_rate=0.00025), metrics=['mae'])
+
+# fit the model and save the weights when training is complete
 dqn.fit(env, nb_steps=1750000, visualize=False, verbose=2)
 dqn.save_weights('policy.h5')
