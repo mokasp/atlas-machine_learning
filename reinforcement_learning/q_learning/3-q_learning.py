3b03e763b968a986136391a72e07cf476a9f9e15
mokasp <kasperbouldin@gmail.com>
2024-09-04 20:01:08 -0500

    (fix) pycodestyle

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -14,24 +14,32 @@
 import numpy as np
 
 
-def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
+def train(env, Q, episodes=5000, max_steps=100, alpha=0.1,
+          gamma=0.99, epsilon=1, min_epsilon=0.1, epsilon_decay=0.05):
     """ function that trains a Q-learning agent on a given environment.
-        
+
         Args:
             env (gym.Env): FrozenLake environment instance.
-            Q (numpy.ndarray): Q-table where rows represent states and columns represent actions.
-            episodes (int): total number of episodes to train over, default is 5000.
-            max_steps (int): maximum number of steps per episode, default is 100.
+            Q (numpy.ndarray): Q-table where rows represent states and columns
+                represent actions.
+            episodes (int): total number of episodes to train over,
+                default is 5000.
+            max_steps (int): maximum number of steps per episode,
+                default is 100.
             alpha (float): learning rate for Q-learning, default is 0.1.
             gamma (float): discount rate for future rewards, default is 0.99.
-            epsilon (float): initial exploration rate for the epsilon-greedy policy, default is 1.
-            min_epsilon (float): minimum value that epsilon should decay to, default is 0.1.
-            epsilon_decay (float): rate at which epsilon decays after each episode, default is 0.05.
-        
+            epsilon (float): initial exploration rate for the epsilon-greedy
+                policy, default is 1.
+            min_epsilon (float): minimum value that epsilon should decay to,
+                default is 0.1.
+            epsilon_decay (float): rate at which epsilon decays after each
+                episode, default is 0.05.
+
         Returns:
             tuple: (Q, total_rewards)
                 - Q (numpy.ndarray): updated Q-table after training.
-                - total_rewards (list): list containing the total reward per episode.
+                - total_rewards (list): list containing the total reward per
+                    episode.
     """
     # keep track of reward at end of each episode
     all_rewards = []
@@ -60,11 +68,14 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1
         # if the agent fell in a hole, subtract from the total reqard
         if end_of_episode and reward == 0:
             reward = -1
-        
+
         # calculate the temporal difference error using the immediate reward
         # and the discounted estimate of the future rewards
-        td_error = (reward + gamma * np.max(Q[current_state, :]) - Q[state, action])
-        
+        td_error = (reward +
+                    gamma *
+                    np.max(Q[current_state, :]) -
+                    Q[state, action])
+
         # update the Q value, set new state, and add new reward to the total
         Q[state, action] = Q[state, action] + alpha * td_error
         state = current_state
@@ -73,10 +84,11 @@ def train(env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1
         # check if the episode is complete
         if end_of_episode:
             break
-        
-        # epsilon decay - as the agent explores throughout training, we can reduce
-        # the epsilon to encourage exploitation
-        epsilon = min_epsilon + (epsilon - min_epsilon) * np.exp(-epsilon_decay * episode)
+
+        # epsilon decay - as the agent explores throughout training, we can
+        # reduce the epsilon to encourage exploitation
+        epsilon = min_epsilon + (epsilon - min_epsilon) * \
+            np.exp(-epsilon_decay * episode)
 
         # keep track of each reward total
         all_rewards.append(rewards)
