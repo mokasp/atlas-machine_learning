8f82b265e7dee825f7d65df09d310bda60912fcd
mokasp <kasperbouldin@gmail.com>
2024-09-04 19:51:43 -0500

    (fix) pycodestyle

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -17,21 +17,21 @@ import numpy as np
 def epsilon_greedy(Q, state, epsilon):
     """ function that selects the next action using the epsilon-greedy
         strategy, it decides whether to explore or  exploit based on epsilon
-    
+
         Args:
             Q (numpy.ndarray): Q-table, where rows represent states and
                 columns represent actions.
             state (int): current state of the environment.
             epsilon (float): probability of selecting a random action
                 (exploration rate).
-    
+
         Returns:
             int: the index of the next action the agent will take.
     """
     # use epsilon-greedy to decide the next move
     # choose a random value between 0 and 1
     choice = np.random.random()
-    
+
     # decide to explore or exploit based on the epsilon
     if choice < epsilon:
         # exploration: pick a random action
