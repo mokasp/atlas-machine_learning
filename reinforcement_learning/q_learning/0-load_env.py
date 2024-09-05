3d36f029ebf09b2ac6f48b49ee5b8c844a3fd200
mokasp <kasperbouldin@gmail.com>
2024-09-04 19:20:43 -0500

    (add) Documentation

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
-""""""
+""" this script contains a function that initializes a FrozenLake environment
+    for a reinforcement learning agent to perfom Q-learning with
+
+    Dependencies:
+        - gym: standard API for reinforcement learning with a diverse collection
+            of reference environments
+    
+    Functions:
+        - load_frozen_lake(desc=None, map_name=None, is_slippery=False): initi-
+            alizes a Frozen Lake environment using the gym library
+"""
 import gym
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
-  """"""
-  return gym.make('FrozenLake-v0', desc=desc, map_name=map_name, is_slippery=is_slippery)
+    """ function that initializes a FrozenLake environment for a reinforcement
+        learning agent to perfom Q-learning with
+
+        Args:
+            desc (optional, list of lists): custom description of the map to
+                load for the environment, or None
+            map_name (optional, string): premade map to load, or None
+            is_slippery (optional, bool): determines if the ice is slippery,
+                default is Fault
+    """
+    return gym.make('FrozenLake-v0', desc=desc, map_name=map_name, is_slippery=is_slippery)
