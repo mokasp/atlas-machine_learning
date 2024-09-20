20b5a15531f6f1e8e3371deb61ffbdb934630bed
mokasp <kasperbouldin@gmail.com>
2024-09-20 05:41:58 -0500

    (add) documentation

--- reinforcement_learning/temporal_differences/0-monte_carlo.py
+++ reinforcement_learning/temporal_differences/0-monte_carlo.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python3
+""" this script contains a function that performs monte carlo algorithm
+    with a gym environment
+
+    Dependencies:
+        - gym: standard API for reinforcement learning with a diverse
+            collection of reference environments
+        - numpy: A library for numerical operations in Python.
+
+    Functions:
+        - monte_carlo(env, V, policy, episodes=5000, max_steps=100,
+            alpha=0.1, gamma=0.99): performs monte carlo algorithm on a given
+            environment.
+"""
 import gym
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+    """ function that performs the monte carlo algorithm
+
+        Args:
+            env (gym.Env): gym environment instance.
+            V (numpy.ndarray): value estimate
+            policy (function): takes in a state and returns the next action to take
+            episodes (int): total number of episodes to train over,
+                default is 5000.
+            max_steps (int): maximum number of steps per episode,
+                default is 100.
+            alpha (float): learning rate for value estimate update, default is 0.1.
+            gamma (float): discount rate for future rewards, default is 0.99.
+
+
+        Returns:
+            np.ndarray: V
+                - V (numpy.ndarray): updated value estimate
+    """
+
 
     for i in range(episodes):
         # reset environment
