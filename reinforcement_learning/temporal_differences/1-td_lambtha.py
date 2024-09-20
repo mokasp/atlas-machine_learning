6bbd6cd22df4b842b3d49d8d3314eff962da2520
mokasp <kasperbouldin@gmail.com>
2024-09-20 05:44:33 -0500

    (fix) pycodestyle

--- reinforcement_learning/temporal_differences/1-td_lambtha.py
+++ reinforcement_learning/temporal_differences/1-td_lambtha.py
@@ -15,19 +15,21 @@ import gym
 import numpy as np
 
 
-def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+def td_lambtha(env, V, policy, lambtha, episodes=5000,
+               max_steps=100, alpha=0.1, gamma=0.99):
     """ function that performs the TD(λ) algorithm
 
         Args:
             env (gym.Env): gym environment instance.
             V (numpy.ndarray): value estimate
-            policy (function): takes in a state and returns the next action to take
+            policy (function): takes in a state and returns the next action
+                to take
             lambtha (float):  eligibility trace factor
             episodes (int): total number of episodes to train over,
                 default is 5000.
             max_steps (int): maximum number of steps per episode,
                 default is 100.
-            alpha (float): learning rate for Q-learning, default is 0.1.
+            alpha (float): learning rate, default is 0.1.
             gamma (float): discount rate for future rewards, default is 0.99.
 
 
@@ -41,11 +43,12 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1,
         state = env.reset()
         end_of_episode = False
 
-        # initialize empty array to keep track of which states have been visited and how many times
+        # initialize empty array to keep track of which states have been
+        # visited and how many times
         eligibilty_trace = np.zeros_like(V)
 
         for _ in range(max_steps):
-            # use policy to decide which action to take 
+            # use policy to decide which action to take
             action = policy(state)
 
             # use selected action and get new reward
