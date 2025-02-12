643713b832fdb8667f97c7bf9653e9293b19a181
mokasp <kasperbouldin@gmail.com>
2025-02-12 12:59:15 -0600

    (change) pycodestyle

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -2,20 +2,23 @@
 """
     Randomly adjusts the brightness of an image.
 
-    This function uses TensorFlow's `random_brightness` method to randomly adjust 
-    the brightness of the input image by a value within the range [-max_delta, max_delta].
+    This function uses TensorFlow's `random_brightness` method to randomly
+    adjust the brightness of the input image by a value within the range
+    [-max_delta, max_delta].
 
     Args:
-        image (Tensor): A 3D tensor representing the image (height, width, channels) with pixel values in the range [0, 1].
-        max_delta (float): The maximum brightness adjustment. The brightness will be randomly adjusted by a factor 
-                           between [-max_delta, max_delta].
+        image (Tensor): A 3D tensor representing the image (height, width,
+        channels) with pixel values in the range [0, 1].
+        max_delta (float): The maximum brightness adjustment. The brightness
+        will be randomly adjusted by a factor between [-max_delta, max_delta]
 
     Returns:
         Tensor: The image with the randomly adjusted brightness.
 
     Example:
         ```
-        image = tf.random.uniform(shape=[256, 256, 3], minval=0, maxval=1, dtype=tf.float32)
+        image = tf.random.uniform(shape=[256, 256, 3], minval=0,
+        maxval=1, dtype=tf.float32)
         adjusted_image = change_brightness(image, 0.2)
         ```
 """
