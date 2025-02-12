e6852aaccbd66f4d5f654f7205a2da64685a8214
mokasp <kasperbouldin@gmail.com>
2025-02-12 13:32:51 -0600

    (change) trying to pass documentation check

--- pipeline/data_augmentation/4-brightness.py
+++ pipeline/data_augmentation/4-brightness.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+
 """ this function uses TensorFlow's random_brightness function to randomly
 adjust the brightness of the input image by a value within the range
 [-max_delta, max_delta] """
+
 import tensorflow as tf
 
 
