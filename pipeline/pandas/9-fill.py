d86060dff0270607c254e32fca3d077681e04d94
mokasp <kasperbouldin@gmail.com>
2025-02-15 16:00:14 -0600

    (change) seperate lines to fill High, Low, and Open columns

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -7,5 +7,7 @@ def fill(df):
     dropped = df.drop('Weighted_Price', axis=1)
     dropped['Close'] = dropped['Close'].fillna(method='ffill')
     dropped['High'] = dropped['High'].fillna(dropped['Close'])
+    dropped['Low'] = dropped['Low'].fillna(dropped['Close'])
+    dropped['Open'] = dropped['Open'].fillna(dropped['Close'])
     dropped[['Volume_(BTC)', 'Volume_(Currency)']] = dropped[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
     return dropped
