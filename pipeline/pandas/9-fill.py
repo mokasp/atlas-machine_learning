f1005179907b748f9c362e37c54ec4413499af92
mokasp <kasperbouldin@gmail.com>
2025-02-15 15:58:45 -0600

    (test) checking fillna works for one column

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -6,6 +6,6 @@ def fill(df):
     """"""
     dropped = df.drop('Weighted_Price', axis=1)
     dropped['Close'] = dropped['Close'].fillna(method='ffill')
-    dropped[['High', 'Low', 'Open']] = dropped[['High', 'Low', 'Open']].fillna(dropped['Close'])
+    dropped['High'] = dropped['High'].fillna(dropped['Close'])
     dropped[['Volume_(BTC)', 'Volume_(Currency)']] = dropped[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
     return dropped
