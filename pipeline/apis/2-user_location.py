cf5cd09890197f63ddb7fe37d5176699120b3910
mokasp <kasperbouldin@gmail.com>
2025-02-19 19:02:50 -0600

    (fix) check status code correctly

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -2,16 +2,20 @@
 """ to be documented, temporary text to check function logic """
 import requests
 import sys
+import datetime
 
 def main():
     """ to be documented, temporary text to check function logic """
     github = sys.argv[1]
-    user_json = requests.get(github).json()
-    if user_json['status'] == '404':
+    user_json = requests.get(github)
+    if user_json.status_code == 404:
         print('Not found')
-    elif user_json['status'] == '403':
-        x = user_json['X-Ratelimit-Reset']
-        print('Reset in ' + str(x) + ' min')
+    elif user_json.status_code == 403:
+        time = user_json.headers['X-Ratelimit-Reset']
+        time = datetime.datetime.fromtimestamp(int(time))
+        cur_time = datetime.datetime.now()
+        minutes = round((time - cur_time).total_seconds() / 60.0)
+        print('Reset in ' + str(minutes) + ' min')
     else:
         print(user_json['location'])
 
