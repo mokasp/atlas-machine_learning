a24f3c04364873d5b1cc6db61dbbb1bb2226f364
mokasp <kasperbouldin@gmail.com>
2025-02-24 13:52:21 -0600

    (fix) pycode style

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -12,19 +12,23 @@ def main():
         if launch_name == 'Galaxy 33 (15R) & 34 (12R)':
             launch_date = launch['date_local']
             rocket_id = launch['rocket']
-            rockets = requests.get('https://api.spacexdata.com/v4/rockets').json()
+            rockets = requests.get(
+                'https://api.spacexdata.com/v4/rockets').json()
             for rocket in rockets:
                 if rocket['id'] == rocket_id:
                     rocket_name = rocket['name']
             launchpad = launch['launchpad']
-            launchpads = requests.get('https://api.spacexdata.com/v4/launchpads').json()
+            launchpads = requests.get(
+                'https://api.spacexdata.com/v4/launchpads').json()
             for pad in launchpads:
                 if pad['id'] == launchpad:
                     launchpad_name = pad['name']
                     launchpad_locality = pad['locality']
-            print(launch_name + ' (' + launch_date + ') ' + rocket_name + ' - ' + launchpad_name + ' (' + launchpad_locality + ')')
+            print(launch_name + ' (' + launch_date + ') ' + rocket_name +
+                  ' - ' + launchpad_name + ' (' + launchpad_locality + ')')
 
             return
+
 
 if __name__ == '__main__':
     main()
