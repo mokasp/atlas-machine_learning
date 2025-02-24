a2faf376b7b1b1a84d6bb39dfe5a123dd6727b29
mokasp <kasperbouldin@gmail.com>
2025-02-24 14:24:24 -0600

    (add) initial logic

---
+++ pipeline/apis/4-rocket_frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" tbw tbw """
+import requests
+
+
+def main():
+    """ tbw tbw """
+    launches = requests.get('https://api.spacexdata.com/v4/launches').json()
+    rockets = requests.get('https://api.spacexdata.com/v4/rockets').json()
+    launches_per_rocket = {}
+    rocket_ids = {}
+    for rocket in rockets:
+        rocket_ids[rocket['id']] = rocket['name']
+        launches_per_rocket[rocket['name']] = 0
+    for launch in launches:
+        rocket_id = launch['rocket']
+        rocket = rocket_ids[rocket_id]
+        launches_per_rocket[rocket] += 1
+    num_launches = dict(sorted(launches_per_rocket.items(), key=lambda item: item[1], reverse=True))
+    for ship in num_launches:
+        if num_launches[ship] > 0:
+            print(ship + ': ' + str(num_launches[ship]))
+
+
+
+if __name__ == '__main__':
+    main()
