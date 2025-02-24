72fe8f511f38be09e1897aa2fce6909f3d4c1391
mokasp <kasperbouldin@gmail.com>
2025-02-24 13:44:46 -0600

    (add) initial logic

---
+++ pipeline/apis/3-first_launch.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" tbw tbw """
+import requests
+
+
+def main():
+    """  tbw tbw  """
+    req = requests.get('https://api.spacexdata.com/v4/launches/')
+    launches = req.json()
+    for launch in reversed(launches):
+        launch_name = launch['name']
+        launch_date = launch['date_local']
+        rocket_id = launch['rocket']
+        rockets = requests.get('https://api.spacexdata.com/v4/rockets').json()
+        for rocket in rockets:
+            if rocket['id'] == rocket_id:
+                rocket_name = rocket['name']
+        launchpad = launch['launchpad']
+        launchpads = requests.get('https://api.spacexdata.com/v4/launchpads').json()
+        for pad in launchpads:
+            if pad['id'] == launchpad:
+                launchpad_name = pad['name']
+                launchpad_locality = pad['locality']
+        print(launch_name + ' (' + launch_date + ') ' + rocket_name + ' - ' + launchpad_name + ' (' + launchpad_locality + ')')
+
+        return
+
+if __name__ == '__main__':
+    main()
