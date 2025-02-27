c2d3ef4b1f3f96ba006d86fd8ef4c4e324d13381
mokasp <kasperbouldin@gmail.com>
2025-02-26 22:53:08 -0600

    (add and fix) pycode style nad documentation

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
-""""""
+""" module that contains a function that sorts the data in reverse
+    chronological order based on the 'Datetime' column, transposes the
+    sorted DataFrame """
 
 
 def flip_switch(df):
+    """
+        function that sorts the data in reverse chronological order based on
+        the 'Datetime' column, transposes the sorted DataFrame, and returns
+        the transformed DataFrame.
+
+        Parameters:
+        -----------
+        df : pandas.DataFrame
+            A DataFrame containing at least a 'Datetime' column.
+
+        Returns:
+        --------
+        pandas.DataFrame
+            The sorted and transposed DataFrame.
+    """
     sorted_df = df.sort_values(by='Timestamp', ascending=False)
     return sorted_df.transpose()
