5b829556e53a6535a3cd3b0d98dece108b0c77aa
mokasp <kasperbouldin@gmail.com>
2025-03-01 15:44:01 -0600

    (fix) exclude entries equal to zero

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE number_of_shows > 0
 GROUP BY genre
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
