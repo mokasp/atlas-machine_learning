a2fb70ddcd51e5dd2f15b867e5fc09dc3b76536d
mokasp <kasperbouldin@gmail.com>
2025-03-01 20:57:25 -0600

    (fic) structure  of file

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -3,25 +3,29 @@
 from pymongo import MongoClient
 
 
+def log_info(collection):
+    """ tbw tbw """
+    tracking = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
+    status = 0
+    logs = 0
+    for log in collection.find():
+        logs += 1
+        request = log['method']
+        if log['path'] == '/status':
+            status += 1
+        if request in tracking.keys():
+            tracking[request] += 1
+
+    print(str(logs) + ' logs')
+    print('Methods:')
+    print('        method GET: ' + str(tracking['GET']))
+    print('        method POST: ' + str(tracking['POST']))
+    print('        method PUT: ' + str(tracking['PUT']))
+    print('        method PATCH: ' + str(tracking['PATCH']))
+    print('        method DELETE: ' + str(tracking['DELETE']))
+    print(str(status) + ' status check')
+
+
 client = MongoClient('mongodb://127.0.0.1:27017')
 collection = client.logs.nginx
-
-tracking = {'GET': 0, 'POST': 0, 'PUT': 0, 'PATCH': 0, 'DELETE': 0}
-status = 0
-logs = 0
-for log in collection.find():
-    logs += 1
-    request = log['method']
-    if log['path'] == '/status':
-        status += 1
-    if request in tracking.keys():
-        tracking[request] += 1
-
-print(str(logs) + ' logs')
-print('Methods:')
-print('        method GET: ' + str(tracking['GET']))
-print('        method POST: ' + str(tracking['POST']))
-print('        method PUT: ' + str(tracking['PUT']))
-print('        method PATCH: ' + str(tracking['PATCH']))
-print('        method DELETE: ' + str(tracking['DELETE']))
-print(str(status) + ' status check')
+log_info(collection)
