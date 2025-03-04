6c662871ad24280bfef6111d7d55863096f2cd24
mokasp <kasperbouldin@gmail.com>
2025-03-04 16:55:58 -0600

    (fix) use coalesce to pick betweet 2020 or the value returned depending on if it is null or not

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,4 +1,4 @@
 -- lists all bans with Glam rock listed as their mainstyle ranked by their lifespan
-SELECT band_name, split - formed AS lifespan
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%';
