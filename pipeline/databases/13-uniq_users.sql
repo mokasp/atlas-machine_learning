dfe57a63e28c96365676a7ee27cc4bb6aebca5c6
mokasp <kasperbouldin@gmail.com>
2025-03-04 16:11:24 -0600

    (add) initial logic and commit

---
+++ pipeline/databases/13-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates the table users if it does not exists
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    name VARCHAR(256)
+)
