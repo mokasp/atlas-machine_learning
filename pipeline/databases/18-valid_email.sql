5a40ad277a3a22f96a1066ed403fba5abc197df6
mokasp <kasperbouldin@gmail.com>
2025-03-05 17:44:53 -0600

    (fix) small adjustment in logic to exclude '+' character

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -5,8 +5,10 @@ CREATE TRIGGER reset_attribute
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
-        SET NEW.valid_email = 1 - NEW.valid_email;
+    IF NEW.email LIKE '%+%' THEN
+        SET NEW.valid_email = 0;
+    ELSEIF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 1;
     END IF;
 END $$
 
